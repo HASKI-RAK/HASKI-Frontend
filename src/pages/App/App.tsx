4efa0ea1356f61de9ab3e30bd32ffb15ff67eb39
David Fischer <davidvfischer@gmail.com>
2022-10-20 15:56:08 +0200

    changed pages behavior added homepage over dashboard tests for home page

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from "@mui/material";
-import { ThemePresentation } from "@pages";
+import { Home, ThemePresentation } from "@pages";
 import { Theme } from "@utils";
 import { Routes } from "react-router";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Dashboard } from "src/components/Dashboard/Dashboard";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
           <Route path="/theme" element={<ThemePresentation />} />
         </Routes>
       </Router>
