669f56dfc41054b02c9dfe4821f88552693f0c2f
DimitriB01 <d.bigler1996@gmx.de>
2022-12-22 09:43:43 +0100

    order test

---
+++ src/shared/auto-order-json-translations.ts
@@ -0,0 +1,17 @@
+﻿const fs = require('fs');
+const path = require('path');
+const english = require('./translation/translationEnglish.json');
+const exec = require('child_process').exec;
+
+const orderedEnglish = {};
+Object.keys(english).sort(function (a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+}).forEach(key => {
+    orderedEnglish[key] = english[key];
+});
+
+fs.writeFile(path.resolve(__dirname, './translation/translationEnglish.json'), JSON.stringify(orderedEnglish, null, '\t'), function (err) {
+    if (err) {
+        throw err;
+    }
+});
