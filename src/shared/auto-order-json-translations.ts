59d64074e00bad5dae6d2b082469eac575da3d14
DimitriB01 <d.bigler1996@gmx.de>
2022-12-22 10:01:00 +0100

    order german too

--- src/shared/auto-order-json-translations.ts
+++ src/shared/auto-order-json-translations.ts
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const english = require('./translation/translationEnglish.json');
+const german = require('./translation/translationGerman.json');
 const exec = require('child_process').exec;
 
 const orderedEnglish = {};
@@ -12,7 +13,20 @@ Object.keys(english).sort(function (a, b) {
     orderedEnglish[key] = english[key];
 });
 
+const orderedGerman = {};
+Object.keys(german).sort(function (a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+}).forEach(key => {
+    orderedGerman[key] = german[key];
+});
+
 fs.writeFile(path.resolve(__dirname, './translation/translationEnglish.json'), JSON.stringify(orderedEnglish, null, '\t'), function (err) {
+    if (err) {
+        throw err;
+    }
+});
+
+fs.writeFile(path.resolve(__dirname, './translation/translationGerman.json'), JSON.stringify(orderedGerman, null, '\t'), function (err) {
     if (err) {
         throw err;
     }
