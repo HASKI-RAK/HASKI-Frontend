e9dc615af55c07e48e95f1e3820d7634808eed3e
DimitriB01 <d.bigler1996@gmx.de>
2022-12-23 07:35:51 +0100

    Update json translation

--- src/components/Questionnaire/TableILS.tsx
+++ src/components/Questionnaire/TableILS.tsx
@@ -1,15 +1,15 @@
 ﻿import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(() => ({
     [`&.${tableCellClasses.head}`]: {
         backgroundColor: '#24262a',
         color: '#FFFFFF',
@@ -23,7 +23,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     }
 }));
 
-const StyledTableCellWithoutBorder = styled(TableCell)(({ theme }) => ({
+const StyledTableCellWithoutBorder = styled(TableCell)(() => ({
     [`&.${tableCellClasses.head}`]: {
         backgroundColor: '#24262a',
         color: '#FFFFFF',
@@ -34,11 +34,11 @@ const StyledTableCellWithoutBorder = styled(TableCell)(({ theme }) => ({
     },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
+const StyledTableRow = styled(TableRow)(() => ({
     '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
+        backgroundColor: 'rgba(200,205,219,0.25)',
     },
-    
+
 }));
 
 
@@ -46,62 +46,67 @@ export function ILS() {
 
     const {t} = useTranslation();
 
-    const inter = new Map<number,string>();
-    inter.set(-1,t("components.QuestionnaireResults.TableILS.balanced"))
-    inter.set(1,t("components.QuestionnaireResults.TableILS.balanced"))
-    inter.set(-3,t("components.QuestionnaireResults.TableILS.balanced"))
-    inter.set(3,t("components.QuestionnaireResults.TableILS.balanced"))
-    inter.set(-5,t("components.QuestionnaireResults.TableILS.moderate"))
-    inter.set(5,t("components.QuestionnaireResults.TableILS.moderate"))
-    inter.set(-7,t("components.QuestionnaireResults.TableILS.moderate"))
-    inter.set(7,t("components.QuestionnaireResults.TableILS.moderate"))
-    inter.set(-9,t("components.QuestionnaireResults.TableILS.strong"))
-    inter.set(9,t("components.QuestionnaireResults.TableILS.strong"))
-    inter.set(-11,t("components.QuestionnaireResults.TableILS.strong"))
-    inter.set(11,t("components.QuestionnaireResults.TableILS.strong"))
-    
+    const inter = new Map<number, string>();
+    inter.set(-1, t("components.QuestionnaireResults.TableILS.balanced"))
+    inter.set(1, t("components.QuestionnaireResults.TableILS.balanced"))
+    inter.set(-3, t("components.QuestionnaireResults.TableILS.balanced"))
+    inter.set(3, t("components.QuestionnaireResults.TableILS.balanced"))
+    inter.set(-5, t("components.QuestionnaireResults.TableILS.moderate"))
+    inter.set(5, t("components.QuestionnaireResults.TableILS.moderate"))
+    inter.set(-7, t("components.QuestionnaireResults.TableILS.moderate"))
+    inter.set(7, t("components.QuestionnaireResults.TableILS.moderate"))
+    inter.set(-9, t("components.QuestionnaireResults.TableILS.strong"))
+    inter.set(9, t("components.QuestionnaireResults.TableILS.strong"))
+    inter.set(-11, t("components.QuestionnaireResults.TableILS.strong"))
+    inter.set(11, t("components.QuestionnaireResults.TableILS.strong"))
+
     function getInterpretation(score: number, interpretationString: string) {
-        if(inter.get(score) === t("components.QuestionnaireResults.TableILS.balanced"))
+        if (inter.get(score) === t("components.QuestionnaireResults.TableILS.balanced"))
             return inter.get(score)
         return inter.get(score) + " " + interpretationString;
     }
 
     const rows = [
-        { id: 1,
-            col1: t("components.QuestionnaireResults.TableILS.dimension"),
+        {
+            id: 1,
+            col1: t("components.QuestionnaireResults.TableILS.Dimension"),
             col2: "",
-            col3: t("components.QuestionnaireResults.TableILS.interpretation"),
-            col4: t("components.QuestionnaireResults.TableILS.score")
+            col3: t("components.QuestionnaireResults.TableILS.Interpretation"),
+            col4: t("components.QuestionnaireResults.TableILS.Score")
         },
-        { id: 2,
-            col1: t("components.QuestionnaireResults.TableILS.active"),
-            col2: t("components.QuestionnaireResults.TableILS.reflective"),
-            col3: getInterpretation(-1,t("components.QuestionnaireResults.TableILS.reflective").toLowerCase()),
+        {
+            id: 2,
+            col1: t("components.QuestionnaireResults.TableILS.Active"),
+            col2: t("components.QuestionnaireResults.TableILS.Reflective"),
+            col3: getInterpretation(-1, t("components.QuestionnaireResults.TableILS.Reflective").toLowerCase()),
             col4: "-1"
         },
-        { id: 3,
-            col1: t("components.QuestionnaireResults.TableILS.sensory"),
-            col2: t("components.QuestionnaireResults.TableILS.intuitive"),
-            col3: getInterpretation(-5,t("components.QuestionnaireResults.TableILS.intuitive").toLowerCase()),
+        {
+            id: 3,
+            col1: t("components.QuestionnaireResults.TableILS.Sensory"),
+            col2: t("components.QuestionnaireResults.TableILS.Intuitive"),
+            col3: getInterpretation(-5, t("components.QuestionnaireResults.TableILS.Intuitive").toLowerCase()),
             col4: "-5"
         },
-        { id: 4,
-            col1: t("components.QuestionnaireResults.TableILS.visual"),
-            col2: t("components.QuestionnaireResults.TableILS.verbal"),
-            col3: getInterpretation(3,t("components.QuestionnaireResults.TableILS.verbal").toLowerCase()),
+        {
+            id: 4,
+            col1: t("components.QuestionnaireResults.TableILS.Visual"),
+            col2: t("components.QuestionnaireResults.TableILS.Verbal"),
+            col3: getInterpretation(3, t("components.QuestionnaireResults.TableILS.Verbal").toLowerCase()),
             col4: "3"
         },
-        { id: 5,
-            col1: t("components.QuestionnaireResults.TableILS.sequential"),
-            col2: t("components.QuestionnaireResults.TableILS.global"),
-            col3: getInterpretation(11,t("components.QuestionnaireResults.TableILS.verbal").toLowerCase()),
+        {
+            id: 5,
+            col1: t("components.QuestionnaireResults.TableILS.Sequential"),
+            col2: t("components.QuestionnaireResults.TableILS.Global"),
+            col3: getInterpretation(11, t("components.QuestionnaireResults.TableILS.Verbal").toLowerCase()),
             col4: "11"
         },
     ];
 
     return (
-        <TableContainer component={Paper} style={{ width: 500 }}>
-            <Table style={{ width: 500 }} aria-label="customized table">
+        <TableContainer component={Paper} style={{width: 500}}>
+            <Table style={{width: 500}} aria-label="customized table">
                 <TableHead>
                     <TableRow>
                         <StyledTableCellWithoutBorder align="left">{rows[0].col1}</StyledTableCellWithoutBorder>
@@ -111,15 +116,15 @@ export function ILS() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.filter(function(row) {
+                    {rows.filter(function (row) {
                         return row.id !== 1;
                     }).map((row) => (
-                            <StyledTableRow>
-                                <StyledTableCellWithoutBorder align="left">{row.col1}</StyledTableCellWithoutBorder>
-                                <StyledTableCellWithoutBorder align="left">{row.col2}</StyledTableCellWithoutBorder>
-                                <StyledTableCell align="left">{row.col3}</StyledTableCell>
-                                <StyledTableCell align="right">{row.col4}</StyledTableCell>
-                            </StyledTableRow>
+                        <StyledTableRow>
+                            <StyledTableCellWithoutBorder align="left">{row.col1}</StyledTableCellWithoutBorder>
+                            <StyledTableCellWithoutBorder align="left">{row.col2}</StyledTableCellWithoutBorder>
+                            <StyledTableCell align="left">{row.col3}</StyledTableCell>
+                            <StyledTableCell align="right">{row.col4}</StyledTableCell>
+                        </StyledTableRow>
                     ))}
                 </TableBody>
             </Table>
--- src/components/Questionnaire/TableListK.tsx
+++ src/components/Questionnaire/TableListK.tsx
@@ -1,15 +1,15 @@
 ﻿import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell)(() => ({
     [`&.${tableCellClasses.head}`]: {
         backgroundColor: '#24262a',
         color: '#FFFFFF',
@@ -23,164 +23,186 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     }
 }));
 
-const StyledTableCellWithoutBorder = styled(TableCell)(({ theme }) => ({
+const StyledTableCellWithoutBorder = styled(TableCell)(() => ({
     [`&.${tableCellClasses.head}`]: {
-      backgroundColor: '#24262a',
-      color: '#FFFFFF',
-      fontSize: 13,
+        backgroundColor: '#24262a',
+        color: '#FFFFFF',
+        fontSize: 13,
     },
     [`&.${tableCellClasses.body}`]: {
-      fontSize: 13,
+        fontSize: 13,
     },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
+const StyledTableRow = styled(TableRow)(() => ({
     '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
+        backgroundColor: 'rgba(200,205,219,0.25)',
     },
-    '&:nth-of-type(2)':{
-      backgroundColor: '#24262a'
+    '&:nth-of-type(1)': {
+        backgroundColor: '#24262a'
     },
-    '&:nth-of-type(7)':{
-      backgroundColor: '#24262a'
+    '&:nth-of-type(6)': {
+        backgroundColor: '#24262a'
     },
 }));
 
 
 export function ListK() {
 
-  const {t} = useTranslation();
-  
+    const {t} = useTranslation();
 
-  const rows = [
-    { id: 1,
-      col1: t("components.QuestionnaireResults.TableListK.factors & subscala"),
-      col2: t("components.QuestionnaireResults.TableListK.score"),
-    },
-    { id: 2,
-      col1: t("components.QuestionnaireResults.TableListK.cognitive strategies"),
-      col2: "2,08",
-      col3: t("components.QuestionnaireResults.TableListK.internal resource management strategies"),
-      col4: "2,33"
-    },
-    { id: 3,
-      col1: t("components.QuestionnaireResults.TableListK.organize"),
-      col2: "2,00",
-      col3: t("components.QuestionnaireResults.TableListK.attention"),
-      col4: "2,67"
-    },
-    { id: 4,
-      col1: t("components.QuestionnaireResults.TableListK.elaborate"),
-      col2: "1,67",
-      col3: t("components.QuestionnaireResults.TableListK.effort"),
-      col4: "3,33"
-    },
-    { id: 5,
-      col1: t("components.QuestionnaireResults.TableListK.critical review"),
-      col2: "1,00",
-      col3: t("components.QuestionnaireResults.TableListK.time"),
-      col4: "1.00"
-    },
-    { id: 6,
-      col1: t("components.QuestionnaireResults.TableListK.repeat"),
-      col2: "3,67",
-      col3: "",
-      col4: ""
-    },
-    { id: 7,
-      col1: t("components.QuestionnaireResults.TableListK.metacognitive strategies"),
-      col2: "1,89",
-      col3: t("components.QuestionnaireResults.TableListK.external resource management strategies"),
-      col4: "3,44"
-    },
-    { id: 8,
-      col1: t("components.QuestionnaireResults.TableListK.aim & plan"),
-      col2: "1,00",
-      col3: t("components.QuestionnaireResults.TableListK.learning with classmates"),
-      col4: "3,00"
-    },
-    { id: 9,
-      col1: t("components.QuestionnaireResults.TableListK.control"),
-      col2: "2,33",
-      col3: t("components.QuestionnaireResults.TableListK.literature research"),
-      col4: "3,67"
-    },
-    { id: 10,
-      col1: t("components.QuestionnaireResults.TableListK.regulate"),
-      col2: "2,33",
-      col3: t("components.QuestionnaireResults.TableListK.learning environment"),
-      col4: "3,67"
-    },
-  ];
 
-  return (
-      <TableContainer component={Paper} style={{ width: 500 }}>
-        <Table style={{ width: 500 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCellWithoutBorder align="left">{rows[0].col1}</StyledTableCellWithoutBorder>
-              <StyledTableCellWithoutBorder align="left">{rows[0].col2}</StyledTableCellWithoutBorder>
-              <StyledTableCellWithoutBorder align="left">{rows[0].col1}</StyledTableCellWithoutBorder>
-              <StyledTableCellWithoutBorder align="left">{rows[0].col2}</StyledTableCellWithoutBorder>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[1].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[1].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[1].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[1].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[2].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[2].col2}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[2].col3}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[2].col4}</StyledTableCellWithoutBorder>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[3].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[3].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[3].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[3].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[4].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[4].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[4].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[4].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[5].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[5].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[5].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[5].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[6].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[6].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[6].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[6].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[7].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[7].col2}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[7].col3}</StyledTableCellWithoutBorder>
-                  <StyledTableCellWithoutBorder align="left" style={{color:"white"}}>{rows[7].col4}</StyledTableCellWithoutBorder>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[8].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[8].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[8].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[8].col4}</StyledTableCell>
-                </StyledTableRow>
-                <StyledTableRow>
-                  <StyledTableCellWithoutBorder align="left">{rows[9].col1}</StyledTableCellWithoutBorder>
-                  <StyledTableCell align="left">{rows[9].col2}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[9].col3}</StyledTableCell>
-                  <StyledTableCell align="left">{rows[9].col4}</StyledTableCell>
-                </StyledTableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-  );
+    const rows = [
+        {
+            id: 1,
+            col1: t("components.QuestionnaireResults.TableListK.Factors & subscales"),
+            col2: t("components.QuestionnaireResults.TableListK.Score"),
+        },
+        {
+            id: 2,
+            col1: t("components.QuestionnaireResults.TableListK.Cognitive strategies"),
+            col2: "2,08",
+            col3: t("components.QuestionnaireResults.TableListK.Internal resource management strategies"),
+            col4: "2,33"
+        },
+        {
+            id: 3,
+            col1: t("components.QuestionnaireResults.TableListK.Organize"),
+            col2: "2,00",
+            col3: t("components.QuestionnaireResults.TableListK.Attention"),
+            col4: "2,67"
+        },
+        {
+            id: 4,
+            col1: t("components.QuestionnaireResults.TableListK.Elaborate"),
+            col2: "1,67",
+            col3: t("components.QuestionnaireResults.TableListK.Effort"),
+            col4: "3,33"
+        },
+        {
+            id: 5,
+            col1: t("components.QuestionnaireResults.TableListK.Critical review"),
+            col2: "1,00",
+            col3: t("components.QuestionnaireResults.TableListK.Time"),
+            col4: "1.00"
+        },
+        {
+            id: 6,
+            col1: t("components.QuestionnaireResults.TableListK.Repeat"),
+            col2: "3,67",
+            col3: "",
+            col4: ""
+        },
+        {
+            id: 7,
+            col1: t("components.QuestionnaireResults.TableListK.Metacognitive strategies"),
+            col2: "1,89",
+            col3: t("components.QuestionnaireResults.TableListK.External resource management strategies"),
+            col4: "3,44"
+        },
+        {
+            id: 8,
+            col1: t("components.QuestionnaireResults.TableListK.Goals & plans"),
+            col2: "1,00",
+            col3: t("components.QuestionnaireResults.TableListK.Learning with classmates"),
+            col4: "3,00"
+        },
+        {
+            id: 9,
+            col1: t("components.QuestionnaireResults.TableListK.Control"),
+            col2: "2,33",
+            col3: t("components.QuestionnaireResults.TableListK.Literature research"),
+            col4: "3,67"
+        },
+        {
+            id: 10,
+            col1: t("components.QuestionnaireResults.TableListK.Regulate"),
+            col2: "2,33",
+            col3: t("components.QuestionnaireResults.TableListK.Learning environment"),
+            col4: "3,67"
+        },
+    ];
+
+    return (
+        <TableContainer component={Paper} style={{width: 500}}>
+            <Table style={{width: 500}} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[0].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left">{rows[0].col2}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left">{rows[0].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left">{rows[0].col2}</StyledTableCellWithoutBorder>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left"
+                                                      style={{color: "white"}}>{rows[1].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left"
+                                                      style={{color: "white"}}>{rows[1].col2}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left"
+                                                      style={{color: "white"}}>{rows[1].col3}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left"
+                                                      style={{color: "white"}}>{rows[1].col4}</StyledTableCellWithoutBorder>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left"
+                        >{rows[2].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left"
+                        >{rows[2].col2}</StyledTableCell>
+                        <StyledTableCell align="left"
+                        >{rows[2].col3}</StyledTableCell>
+                        <StyledTableCell align="left"
+                        >{rows[2].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[3].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[3].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[3].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[3].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[4].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[4].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[4].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[4].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[5].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[5].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[5].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[5].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left" style={{color: "white"}}>
+                            {rows[6].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left" style={{color: "white"}}>
+                            {rows[6].col2}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left" style={{color: "white"}}>
+                            {rows[6].col3}</StyledTableCellWithoutBorder>
+                        <StyledTableCellWithoutBorder align="left" style={{color: "white"}}>
+                            {rows[6].col4}</StyledTableCellWithoutBorder>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[7].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[7].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[7].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[7].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[8].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[8].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[8].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[8].col4}</StyledTableCell>
+                    </StyledTableRow>
+                    <StyledTableRow>
+                        <StyledTableCellWithoutBorder align="left">{rows[9].col1}</StyledTableCellWithoutBorder>
+                        <StyledTableCell align="left">{rows[9].col2}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[9].col3}</StyledTableCell>
+                        <StyledTableCell align="left">{rows[9].col4}</StyledTableCell>
+                    </StyledTableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
