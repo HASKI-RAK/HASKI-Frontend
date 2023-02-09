8b4c419f69eff0eab932c97359e5098c330746ce
DimitriB01 <d.bigler1996@gmx.de>
2023-02-09 12:24:36 +0100

    import shortened

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,13 +2,11 @@ import { Dashboard } from "./Dashboard/Dashboard";
 import { DropdownLanguage } from "./DropdownLanguage/DropdownLanguage";
 import { TableILS } from "./Questionnaire/TableILS";
 import { GraphILS } from "./Questionnaire/GraphILS";
-import { ResultDescriptionILS } from "./Questionnaire/ResultDescriptionILS";
+import { ResultDescILS, ResultDescriptionILS } from "./Questionnaire/ResultDescriptionILS";
 import { TableListK } from "./Questionnaire/TableListK";
 import { GraphListK } from "./Questionnaire/GraphListK";
-import { ResultDescriptionListK } from "./Questionnaire/ResultDescriptionListK";
+import { ResultDescListK, ResultDescriptionListK } from "./Questionnaire/ResultDescriptionListK";
 import { QuestionnaireResultsButton } from "./Questionnaire/QuestionnaireResultsButton";
-import { ResultDescILS } from "./Questionnaire/ResultDescriptionILS";
-import { ResultDescListK } from "./Questionnaire/ResultDescriptionListK";
 import { Text } from "./Text/Text";
 /**
  * Unique components. Structure as follows: <componentname>/<componentname.tsx>, <componentname.test.tsx>, ...
