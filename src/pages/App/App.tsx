d37e86e60e35276cef12d0b907c289865dc5a770
DimitriB01 <d.bigler1996@gmx.de>
2023-02-24 11:52:06 +0100

    added tests for Ringbuffer and log functionality

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -6,34 +6,34 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import log from "loglevel";
 import {RingBuffer} from "../../shared/RingBuffer";
 
-const GlobalRingBuffer = new RingBuffer<[string, string]>(100);
-if(localStorage.getItem('ringBufferContent') !== null){
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore - localStorage.getItem('ringBufferContent') is not null
-    const localStorageRingBuffer = JSON.parse(localStorage.getItem('ringBufferContent'));
-    GlobalRingBuffer.fromArray(localStorageRingBuffer.buffer)
-}
-else{
-    localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
-}
-
-const originalFactory = log.methodFactory;
-log.methodFactory = function (methodName, logLevel, loggerName) {
-    const rawMethod = originalFactory(methodName, logLevel, loggerName);
-
-    return function (message) {
-        //show only warnings and error in console and log everything else in the GlobalRingBuffer?
-        if(methodName==="warn" || methodName==="error"){
-            rawMethod(message);
-            console.log(GlobalRingBuffer);
-        }
-        GlobalRingBuffer.add([(new Date()).toUTCString(), message]);
-        localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
-    };
-};
-
-
 function App() {
+
+    const GlobalRingBuffer = new RingBuffer<[string, string]>(100);
+    if(localStorage.getItem('ringBufferContent') !== null){
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore - localStorage.getItem('ringBufferContent') is not null
+        const localStorageRingBuffer = JSON.parse(localStorage.getItem('ringBufferContent'));
+        GlobalRingBuffer.fromArray(localStorageRingBuffer.buffer)
+    }
+    else{
+        localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
+    }
+
+    const originalFactory = log.methodFactory;
+    log.methodFactory = function (methodName, logLevel, loggerName) {
+        const rawMethod = originalFactory(methodName, logLevel, loggerName);
+
+        return function (message) {
+            //show only warnings and error in console and log everything else in the GlobalRingBuffer?
+            if(methodName==="warn" || methodName==="error"){
+                rawMethod(message);
+                console.log(GlobalRingBuffer);
+            }
+            GlobalRingBuffer.add([(new Date()).toUTCString(), message]);
+            localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
+        };
+    };
+
   return (
     <ThemeProvider theme={Theme}>
       <Router>
