94bbf1f932049dc13b7e97f827313b955320b48c
DimitriB01 <d.bigler1996@gmx.de>
2023-02-27 13:06:42 +0100

    Buffer initialisation and log config is now in shared folder

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -3,36 +3,11 @@ import { Home, ThemePresentation } from "@pages";
 import { Theme } from "@utils";
 import { Routes } from "react-router";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import log from "loglevel";
-import {RingBuffer} from "../../shared/RingBuffer";
+import {logBuffer} from "../../shared/logBuffer.config";
 
 function App() {
 
-    const GlobalRingBuffer = new RingBuffer<[string, string]>(100);
-    if(localStorage.getItem('ringBufferContent') !== null){
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore - localStorage.getItem('ringBufferContent') is not null
-        const localStorageRingBuffer = JSON.parse(localStorage.getItem('ringBufferContent'));
-        GlobalRingBuffer.fromArray(localStorageRingBuffer.buffer)
-    }
-    else{
-        localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
-    }
-
-    const originalFactory = log.methodFactory;
-    log.methodFactory = function (methodName, logLevel, loggerName) {
-        const rawMethod = originalFactory(methodName, logLevel, loggerName);
-
-        return function (message) {
-            //show only warnings and error in console and log everything else in the GlobalRingBuffer?
-            if(methodName==="warn" || methodName==="error"){
-                rawMethod(message);
-                console.log(GlobalRingBuffer);
-            }
-            GlobalRingBuffer.add([(new Date()).toUTCString(), message]);
-            localStorage.setItem('ringBufferContent', JSON.stringify(GlobalRingBuffer));
-        };
-    };
+  logBuffer();
 
   return (
     <ThemeProvider theme={Theme}>
---
+++ src/shared/logBuffer.config.test.tsx
@@ -0,0 +1,52 @@
+import {render} from "@testing-library/react";
+import {App} from "@pages";
+import log from "loglevel";
+
+describe("Test the demo component", () => {
+
+    test("log messages are added to localStorage", () => {
+        render(<App/>);
+        log.setLevel("trace");
+        log.trace("This is a trace");
+        log.warn("This is a warning");
+        log.error("This is an error");
+        const currentDate = new Date().toUTCString();
+
+        expect(localStorage.getItem('ringBufferContent')).not.toBeNull();
+        expect(localStorage.getItem('ringBufferContent')).toContain(currentDate);
+        expect(localStorage.getItem('ringBufferContent')).toContain("This is a warning");
+        expect(localStorage.getItem('ringBufferContent')).toContain("This is an error");
+        expect(localStorage.getItem('ringBufferContent')).toContain("This is a trace");
+    });
+
+});
+
+
+describe("localStorage already set", () => {
+
+    beforeEach(() => {
+        Object.defineProperty(window, "localStorage", {
+            value: {
+                getItem: jest.fn(() => '{"buffer": ["mockedValue"]}'),
+                setItem: localStorage.setItem.bind(localStorage),
+                removeItem: jest.fn(),
+            },
+            writable: true,
+        });
+        localStorage.setItem("ringBufferContent", '{"buffer": ["mockedValue"]}');
+    });
+
+    test("should return a value from GlobalRingBuffer", () => {
+
+        render(<App/>);
+        log.setLevel("trace");
+        log.trace("This is a trace");
+        log.warn("This is a warning");
+        log.error("This is an error");
+
+        expect(localStorage.getItem('ringBufferContent')).not.toBeNull();
+        expect(localStorage.getItem('ringBufferContent')).toContain("{\"buffer\": [\"mockedValue\"]}");
+
+    });
+
+});
