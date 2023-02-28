9ab072b90221ae77b5fcdad62e147b67f2cdc818
DimitriB01 <d.bigler1996@gmx.de>
2023-02-28 13:28:34 +0100

    Added log messages

--- src/components/DropdownLanguage/DropdownLanguage.tsx
+++ src/components/DropdownLanguage/DropdownLanguage.tsx
@@ -6,18 +6,24 @@ import log from "loglevel";
 
 
 export const DropdownLanguage = () => {
-    
+
+    log.setLevel("trace");
     const {i18n} = useTranslation();
     const startingLanguage = localStorage.getItem("i18nextLng") as string;
-    
-    function onClickLanguageChange(e: { target: { value: string }; }){
-        i18n.changeLanguage(e.target.value);
-        localStorage.setItem("i18nextLng", e.target.value);
-        log.setLevel("trace");
-        log.trace("this is a trace");
-        log.debug("this is a debug");
-        log.warn("this is a warning");
-        log.error("this is an error");
+    function onClickLanguageChange(e: { target: { value: string }; }) {
+        try {
+            i18n.changeLanguage(e.target.value);
+            localStorage.setItem("i18nextLng", e.target.value);
+            log.trace("The language was changed to: " + e.target.value);
+        }
+        catch(e: unknown){
+            if(typeof e === "string"){
+                log.error("The language could not be changed. Error Message: "+e);
+            }
+            else if(e instanceof Error){
+                log.error("The language could not be changed. Error Message: "+e.message);
+            }
+        }
     }
     
     return(
@@ -32,5 +38,4 @@ export const DropdownLanguage = () => {
             </Select>
         </div>
     )
-    /*The display-text is only an example to show how the internationalization works*/
 };
