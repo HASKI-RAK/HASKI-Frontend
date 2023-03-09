30c32981eed537cd270e17b05b8c963dfc789deb
DimitriB01 <d.bigler1996@gmx.de>
2023-03-09 11:11:06 +0100

    removed import react

--- src/components/Questionnaire/TableILS.tsx
+++ src/components/Questionnaire/TableILS.tsx
@@ -1,5 +1,4 @@
-﻿import * as React from 'react';
-import Table from '@mui/material/Table';
+﻿import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
