fe109d0d8b169055f40a7307af3fb561ff8f1668
David Fischer <davidvfischer@gmail.com>
2023-03-21 15:54:31 +0100

    added dimi description

--- src/components/DropdownLanguage/DropdownLanguage.tsx
+++ src/components/DropdownLanguage/DropdownLanguage.tsx
@@ -8,7 +8,32 @@ import log from "loglevel";
  * 
  * @remarks
  * The language is stored in the local storage and is used to set the language of the application.
- * 
+ * ```ts 
+import {useTranslation} from "react-i18next";
+                                             
+export const Text = () => {
+    const {t, i18n} = useTranslation();
+    return(
+        <div>
+            <div>
+                Current Language: {i18n.language} <br/>
+                {(t("previousText"))} <br/>
+                {(t("nextText"))} <br/>
+                {(t("spellcheckText"))} <br/>
+           </div>
+        </div>
+    )
+};
+```
+| Language: German | Language: English |
+|--|--|
+| <img src="https://user-images.githubusercontent.com/92084464/207361066-d8aafa32-7506-4fdd-9242-f2356a4f6d0c.png" width="150"/> | <img src="https://user-images.githubusercontent.com/92084464/207364081-736c6f37-db9a-4db5-9ca7-5432788e072e.png" width="150"/> |
+If a component uses i18n or useTranslation(), then i18n must be mocked in the corresponding Jest test. The mock is placed above the describe statement.
+```ts
+jest.mock('react-i18next', () => ({
+    useTranslation: () => ({t: (key:string) => key})
+}));
+```
  * @returns {JSX.Element} - The DropdownLanguage component.
  */
 export const DropdownLanguage = () => {
