7182e2f2d8c7a179b4f036b120fca7cbe21f21b1
DimitriB01 <d.bigler1996@gmx.de>
2023-04-06 16:01:07 +0200

    changes to match the future api-get-call better

--- src/components/LocalNav/LocalNav.tsx
+++ src/components/LocalNav/LocalNav.tsx
@@ -16,6 +16,245 @@ import {useTranslation} from "react-i18next";
  *
  * @category Components
  */
+
+//todo: get Topics of Student-id from backend
+    //todo: get paths of topics from backend, and sort with .sort((a, b) => a.position - b.position);
+
+const responseTopics = {
+    "topics": [
+        {
+            "id": 1,
+            "name": "Allgemeine Informationen",
+            "lms_id": 1,
+            "is_topic": true,
+            "parent_id": 0,
+            "contains_le": true,
+            "done": false,
+            "done_percantage": 0,
+            "last_visit": "0",
+            "time_spend": 0,
+            "is_recommended": true
+        },
+        {
+            "id": 2,
+            "name": "Adapter",
+            "lms_id": 2,
+            "is_topic": true,
+            "parent_id": 0,
+            "contains_le": true,
+            "done": false,
+            "done_percantage": 0,
+            "last_visit": "0",
+            "time_spend": 0,
+            "is_recommended": false
+        },
+        {
+            "id": 3,
+            "name": "Command, Command with Undo, Command Processor",
+            "lms_id": 3,
+            "is_topic": true,
+            "parent_id": 0,
+            "contains_le": true,
+            "done": false,
+            "done_percantage": 0,
+            "last_visit": "0",
+            "time_spend": 0,
+            "is_recommended": false
+        },
+        {
+            "id": 4,
+            "name": "Strategie",
+            "lms_id": 4,
+            "is_topic": true,
+            "parent_id": 0,
+            "contains_le": true,
+            "done": false,
+            "done_percantage": 0,
+            "last_visit": "0",
+            "time_spend": 0,
+            "is_recommended": false
+        },
+        {
+            "id": 5,
+            "name": "Zustand",
+            "lms_id": 5,
+            "is_topic": true,
+            "parent_id": 0,
+            "contains_le": true,
+            "done": false,
+            "done_percantage": 0,
+            "last_visit": "0",
+            "time_spend": 0,
+            "is_recommended": false
+        },
+    ]
+};
+
+const responseLearningElements = [
+    {
+        "position": 1,
+        "learning_element": {
+            "id": 1,
+            "lms_id": 14,
+            "activity_type": "h5pactiviy",
+            "classification": "SE",
+            "name": "Selbsteinschätzungstest",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 2,
+        "learning_element": {
+            "id": 2,
+            "lms_id": 63,
+            "activity_type": "h5pactiviy",
+            "classification": "KÜ",
+            "name": "Kurzübersicht",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 4,
+        "learning_element": {
+            "id": 3,
+            "lms_id": 62,
+            "activity_type": "h5pactiviy",
+            "classification": "BE",
+            "name": "Beispiel",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 3,
+        "learning_element": {
+            "id": 4,
+            "lms_id": 15,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 1 Leicht",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 5,
+        "learning_element": {
+            "id": 5,
+            "lms_id": 68,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 2 Leicht",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 6,
+        "learning_element": {
+            "id": 6,
+            "lms_id": 66,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 1 Mittel",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 7,
+        "learning_element": {
+            "id": 7,
+            "lms_id": 69,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 2 Mittel",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 8,
+        "learning_element": {
+            "id": 8,
+            "lms_id": 67,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 1 Schwer",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 9,
+        "learning_element": {
+            "id": 9,
+            "lms_id": 20,
+            "activity_type": "h5pactiviy",
+            "classification": "ÜB",
+            "name": "Uebung 2 Schwer",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    },
+    {
+        "position": 10,
+        "learning_element": {
+            "id": 10,
+            "lms_id": 71,
+            "activity_type": "h5pactiviy",
+            "classification": "ZF",
+            "name": "Zusammenfassung",
+            "done": false,
+            "done_at": "",
+            "nr_of_visits": 0,
+            "last_visit": "",
+            "time_spend": 0,
+            "is_recommended": true
+        }
+    }
+];
+
+responseLearningElements.sort((a, b) => a.position - b.position);
+
 const LocalNav = () => {
     const {t} = useTranslation();
     return (
@@ -24,111 +263,25 @@ const LocalNav = () => {
             <Divider/>
             {/** TODO 📑 add real topics */}
             <div>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography variant="h6">{t("components.LocalNav.LocalNav.Topics.1")}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.1")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.2")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.3")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.4")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.5")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.6")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.7")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.8")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.9")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.10")}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header"
-                    >
-                        <Typography variant="h6">{t("components.LocalNav.LocalNav.Topics.2")}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.1")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.2")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.3")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.4")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.5")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.6")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.7")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.8")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.9")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.10")}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header"
-                    >
-                        <Typography variant="h6">{t("components.LocalNav.LocalNav.Topics.3")}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.1")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.2")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.3")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.4")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.5")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.6")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.7")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.8")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.9")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.10")}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header"
-                    >
-                        <Typography variant="h6">{t("components.LocalNav.LocalNav.Topics.4")}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.1")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.2")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.3")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.4")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.5")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.6")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.7")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.8")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.9")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.10")}</Typography>
-                    </AccordionDetails>
-                </Accordion>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel2a-content"
-                        id="panel2a-header"
-                    >
-                        <Typography variant="h6">{t("components.LocalNav.LocalNav.Topics.5")}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.1")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.2")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.3")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.4")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.5")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.6")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.7")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.8")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.9")}</Typography>
-                        <Typography variant="body2">{t("components.LocalNav.LocalNav.Topics.X.10")}</Typography>
-                    </AccordionDetails>
-                </Accordion>
+                <>
+                {responseTopics.topics.map((topic) => (
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon/>}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography variant="h6">{topic.name}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            {/*api-call get all learning elements from current topic and sort it with position*/}
+                            {responseLearningElements.map((learningElement) => (
+                                <Typography variant="body2">{learningElement.position} {learningElement.learning_element.name}</Typography>
+                            ))}
+                        </AccordionDetails>
+                    </Accordion>
+                ))}
+                </>
             </div>
         </Box>
     );
--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -21,84 +21,244 @@ import HelpIcon from "@mui/icons-material/Help";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { useTranslation } from "react-i18next";
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-const topics = [
+const settings = ["Logout"];
+
+const responseTopics = {
+  "topics": [
+    {
+      "id": 1,
+      "name": "Allgemeine Informationen",
+      "lms_id": 1,
+      "is_topic": true,
+      "parent_id": 0,
+      "contains_le": true,
+      "done": false,
+      "done_percantage": 0,
+      "last_visit": "0",
+      "time_spend": 0,
+      "is_recommended": true
+    },
+    {
+      "id": 2,
+      "name": "Adapter",
+      "lms_id": 2,
+      "is_topic": true,
+      "parent_id": 0,
+      "contains_le": true,
+      "done": false,
+      "done_percantage": 0,
+      "last_visit": "0",
+      "time_spend": 0,
+      "is_recommended": false
+    },
+    {
+      "id": 3,
+      "name": "Command, Command with Undo, Command Processor",
+      "lms_id": 3,
+      "is_topic": true,
+      "parent_id": 0,
+      "contains_le": true,
+      "done": false,
+      "done_percantage": 0,
+      "last_visit": "0",
+      "time_spend": 0,
+      "is_recommended": false
+    },
+    {
+      "id": 4,
+      "name": "Strategie",
+      "lms_id": 4,
+      "is_topic": true,
+      "parent_id": 0,
+      "contains_le": true,
+      "done": false,
+      "done_percantage": 0,
+      "last_visit": "0",
+      "time_spend": 0,
+      "is_recommended": false
+    },
+    {
+      "id": 5,
+      "name": "Zustand",
+      "lms_id": 5,
+      "is_topic": true,
+      "parent_id": 0,
+      "contains_le": true,
+      "done": false,
+      "done_percantage": 0,
+      "last_visit": "0",
+      "time_spend": 0,
+      "is_recommended": false
+    },
+  ]
+};
+
+responseTopics.topics.sort((a, b) => b.id - a.id);
+
+const responseLearningElements = [
   {
-    name: ("components.LocalNav.LocalNav.Topics.5"),
-    subtopics: [
-      "components.LocalNav.LocalNav.Topics.X.1",
-      "components.LocalNav.LocalNav.Topics.X.2",
-      "components.LocalNav.LocalNav.Topics.X.3",
-      "components.LocalNav.LocalNav.Topics.X.4",
-      "components.LocalNav.LocalNav.Topics.X.5",
-      "components.LocalNav.LocalNav.Topics.X.6",
-      "components.LocalNav.LocalNav.Topics.X.7",
-      "components.LocalNav.LocalNav.Topics.X.8",
-      "components.LocalNav.LocalNav.Topics.X.9",
-      "components.LocalNav.LocalNav.Topics.X.10",
-    ],
+    "position": 1,
+    "learning_element": {
+      "id": 1,
+      "lms_id": 14,
+      "activity_type": "h5pactiviy",
+      "classification": "SE",
+      "name": "Selbsteinschätzungstest",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
   },
   {
-    name: ("components.LocalNav.LocalNav.Topics.4"),
-    subtopics: [
-      "components.LocalNav.LocalNav.Topics.X.1",
-      "components.LocalNav.LocalNav.Topics.X.2",
-      "components.LocalNav.LocalNav.Topics.X.3",
-      "components.LocalNav.LocalNav.Topics.X.4",
-      "components.LocalNav.LocalNav.Topics.X.5",
-      "components.LocalNav.LocalNav.Topics.X.6",
-      "components.LocalNav.LocalNav.Topics.X.7",
-      "components.LocalNav.LocalNav.Topics.X.8",
-      "components.LocalNav.LocalNav.Topics.X.9",
-      "components.LocalNav.LocalNav.Topics.X.10",
-    ],
+    "position": 2,
+    "learning_element": {
+      "id": 2,
+      "lms_id": 63,
+      "activity_type": "h5pactiviy",
+      "classification": "KÜ",
+      "name": "Kurzübersicht",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
   },
   {
-    name: ("components.LocalNav.LocalNav.Topics.3"),
-    subtopics: [
-      "components.LocalNav.LocalNav.Topics.X.1",
-      "components.LocalNav.LocalNav.Topics.X.2",
-      "components.LocalNav.LocalNav.Topics.X.3",
-      "components.LocalNav.LocalNav.Topics.X.4",
-      "components.LocalNav.LocalNav.Topics.X.5",
-      "components.LocalNav.LocalNav.Topics.X.6",
-      "components.LocalNav.LocalNav.Topics.X.7",
-      "components.LocalNav.LocalNav.Topics.X.8",
-      "components.LocalNav.LocalNav.Topics.X.9",
-      "components.LocalNav.LocalNav.Topics.X.10",
-    ],
+    "position": 4,
+    "learning_element": {
+      "id": 3,
+      "lms_id": 62,
+      "activity_type": "h5pactiviy",
+      "classification": "BE",
+      "name": "Beispiel",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
   },
   {
-    name: ("components.LocalNav.LocalNav.Topics.2"),
-    subtopics: [
-      "components.LocalNav.LocalNav.Topics.X.1",
-      "components.LocalNav.LocalNav.Topics.X.2",
-      "components.LocalNav.LocalNav.Topics.X.3",
-      "components.LocalNav.LocalNav.Topics.X.4",
-      "components.LocalNav.LocalNav.Topics.X.5",
-      "components.LocalNav.LocalNav.Topics.X.6",
-      "components.LocalNav.LocalNav.Topics.X.7",
-      "components.LocalNav.LocalNav.Topics.X.8",
-      "components.LocalNav.LocalNav.Topics.X.9",
-      "components.LocalNav.LocalNav.Topics.X.10",
-    ],
+    "position": 3,
+    "learning_element": {
+      "id": 4,
+      "lms_id": 15,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 1 Leicht",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
   },
   {
-    name: ("components.LocalNav.LocalNav.Topics.1"),
-    subtopics: [
-      "components.LocalNav.LocalNav.Topics.X.1",
-      "components.LocalNav.LocalNav.Topics.X.2",
-      "components.LocalNav.LocalNav.Topics.X.3",
-      "components.LocalNav.LocalNav.Topics.X.4",
-      "components.LocalNav.LocalNav.Topics.X.5",
-      "components.LocalNav.LocalNav.Topics.X.6",
-      "components.LocalNav.LocalNav.Topics.X.7",
-      "components.LocalNav.LocalNav.Topics.X.8",
-      "components.LocalNav.LocalNav.Topics.X.9",
-      "components.LocalNav.LocalNav.Topics.X.10",
-    ],
+    "position": 5,
+    "learning_element": {
+      "id": 5,
+      "lms_id": 68,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 2 Leicht",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
   },
+  {
+    "position": 6,
+    "learning_element": {
+      "id": 6,
+      "lms_id": 66,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 1 Mittel",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
+  },
+  {
+    "position": 7,
+    "learning_element": {
+      "id": 7,
+      "lms_id": 69,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 2 Mittel",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
+  },
+  {
+    "position": 8,
+    "learning_element": {
+      "id": 8,
+      "lms_id": 67,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 1 Schwer",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
+  },
+  {
+    "position": 9,
+    "learning_element": {
+      "id": 9,
+      "lms_id": 20,
+      "activity_type": "h5pactiviy",
+      "classification": "ÜB",
+      "name": "Uebung 2 Schwer",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
+  },
+  {
+    "position": 10,
+    "learning_element": {
+      "id": 10,
+      "lms_id": 71,
+      "activity_type": "h5pactiviy",
+      "classification": "ZF",
+      "name": "Zusammenfassung",
+      "done": false,
+      "done_at": "",
+      "nr_of_visits": 0,
+      "last_visit": "",
+      "time_spend": 0,
+      "is_recommended": true
+    }
+  }
 ];
+
+responseLearningElements.sort((a, b) => a.position - b.position);
 
 /**
  * The MenuBar component is the top bar of the application.
@@ -217,7 +377,7 @@ const MenuBar = () => {
               >
                 <Box sx={{ p: 2 }}>
                   <Grid container direction="column-reverse" spacing={2}>
-                    {topics.map((topic) => (
+                    {responseTopics.topics.map((topic) => (
                       <>
                         <Grid item xs={12} key={t(topic.name)}>
                           <Typography variant="h6">{t(topic.name)}</Typography>
@@ -229,25 +389,25 @@ const MenuBar = () => {
                               justifyContent: "start",
                             }}
                           >
-                            {topic.subtopics.map((subtopic) => (
+                            {responseLearningElements.map((element) => (
                               <Link
-                                key={t(subtopic)}
+                                key={element.learning_element.name}
                                 underline="hover"
                                 variant="body2"
                                 component="span"
                                 color="inherit"
                                 sx={{ m: 1 }}
                                 onClick={() => {
-                                  navigate(`/topics/${t(topic.name)}/${t(subtopic)}`);
+                                  navigate(`/topics/${t(topic.name)}/${t(element.learning_element.name)}`);
                                   handleCloseTopicsMenu();
                                 }}
                               >
-                                {t(subtopic)}
+                                {element.learning_element.name}
                               </Link>
                             ))}
                           </Box>
                         </Grid>
-                        {topics.indexOf(topic) !== topics.length - 1 && (
+                        {responseTopics.topics.indexOf(topic) !== responseTopics.topics.length - 1 && (
                           <Divider flexItem />
                         )}
                       </>
