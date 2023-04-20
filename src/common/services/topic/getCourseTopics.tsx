f9df9c3b12c94a2db268ec292df7be5b455e5bbc
DimitriB01 <d.bigler1996@gmx.de>
2023-04-20 13:48:49 +0200

    hook added, services added, environment changed to localhost

---
+++ src/common/services/topic/getCourseTopics.tsx
@@ -0,0 +1,21 @@
+import { RequestResponse } from "./RequestResponse";
+
+export const getCourseTopics = async (): Promise<RequestResponse> => {
+    const response = await fetch(
+        process.env.BACKEND + `/user/2/5/student/1/course/1/topic`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "text/json",
+            },
+        }
+    );
+
+    const data = await response.json();
+
+    return {
+        status: response.status,
+        message: response.statusText,
+        data: data,
+    };
+};
--- src/components/MenuBar/MenuBar.hooks.tsx
+++ src/components/MenuBar/MenuBar.hooks.tsx
@@ -1,141 +1,43 @@
 import {useEffect, useState} from "react";
+import {getCourseTopics} from "../../common/services/topic/getCourseTopics";
+import {getElementLearningPath} from "../../common/services/learningPath/getElementLearningPath";
+import {LearningPath} from "../../common/services/learningPath/RequestResponse";
+import {Topic} from "../../common/services/topic/RequestResponse";
 
-export type Topic = {
-    contains_le: boolean;
-    created_at: string;
-    created_by: string;
-    id: number;
-    is_topic: boolean;
-    last_updated: string | null;
-    lms_id: number;
-    name: string;
-    parent_id: number | null;
-    student_topic: {
-        done: boolean;
-        done_at: string | null;
-        id: number;
-        student_id: number;
-        topic_id: number;
-        visits: string[]; // Define type of visits array if known
-    };
-    university: string;
-}
+const getSortedLearningPath = async (data: Topic[]): Promise<LearningPath[]> => {
+    const promises = data.map((topic) => getElementLearningPath(topic.id));
+    const learningPaths = await Promise.all(promises);
 
-export type TopicsResponse = {
-    topics: Topic[];
-}
-
-export type LearningElement = {
-    activity_type: string;
-    classification: string;
-    created_at: string;
-    created_by: string;
-    id: number;
-    last_updated: string | null;
-    lms_id: number;
-    name: string;
-    student_learning_element: null;
-    university: string;
-}
-
-export type PathItem = {
-    id: number;
-    learning_element: LearningElement;
-    learning_element_id: number;
-    learning_path_id: number;
-    position: number;
-    recommended: boolean;
-}
-
-export type LearningPath = {
-    based_on: string;
-    calculated_on: string | null;
-    course_id: number;
-    id: number;
-    path: PathItem[];
-}
+    return learningPaths
+    .filter((learningPath) => learningPath.status === 200)
+     .map((learningPath) => {
+        learningPath.data.path.sort((a, b) => a.position - b.position);
+        return learningPath.data;
+    });
+};
 
 export const useLearningPath = () => {
     const [loading, setLoading] = useState(true);
     const [topics, setTopics] = useState<Topic[]>([]);
     const [learningPath, setLearningPath] = useState<LearningPath[]>([]);
 
-    useEffect(() => {
-        const fetchTopics = async() => {
-            setLoading(true); // set loading state to true before making request
-            try {
-                const responseTopic = await fetch('http://127.0.0.1:5000/user/2/5/student/1/course/1/topic');
-                const dataTopic: TopicsResponse = await responseTopic.json();
-                dataTopic.topics.sort((a, b) => b.id - a.id);
-                setTopics(dataTopic.topics);
+    const effect = async ()  => {
 
-                try {
-                    const dataLearningPath: LearningPath[] = [];
-                    for(const topic of dataTopic.topics) {
-                        const topicIndex = topic.id.toString();
-                        const responseLearningPath = await fetch(`http://127.0.0.1:5000/user/2/5/student/1/course/1/topic/${topicIndex}/learningPath`);
-                        const path: LearningPath = await responseLearningPath.json();
-                        // how do we know where the element is in Moodle?
-                        path.path.sort((a, b) => a.position - b.position);
-                        dataLearningPath.push(path);
-                    }
+        setLoading(true);
+        getCourseTopics().then((response) => {
+            if(response.status === 200) {
+                setTopics(response.data.topics);
+
+                getSortedLearningPath(response.data.topics).then((dataLearningPath) => {
                     setLearningPath(dataLearningPath);
-                    setLoading(false); // set loading state to false after request is done
-                }
-                catch(error) {
-                    console.error(error);
-                    const emptyPath: LearningPath[] = [{
-                        based_on: "",
-                        calculated_on: "",
-                        course_id: 0,
-                        id: 0,
-                        path: [
-                            {
-                                id: 0,
-                                learning_element: {
-                                    activity_type: "",
-                                    classification: "",
-                                    created_at: "",
-                                    created_by: "",
-                                    id: 0,
-                                    last_updated: "",
-                                    lms_id: 0,
-                                    name: "",
-                                    student_learning_element: null,
-                                    university: ""
-                                },
-                                learning_element_id: 0,
-                                learning_path_id: 0,
-                                position: 0,
-                                recommended: false
-                            }
-                        ]
-                    }];
-                    setLearningPath(emptyPath);
-                    setLoading(false); // set loading state to false if there is an error
-                }
+                    setLoading(false);
+                })
             }
-            catch(error) {
-                console.error(error);
-                const emptyTopic: Topic[] = [{
-                    contains_le: false,
-                    created_at: "",
-                    created_by: "",
-                    id: 0,
-                    is_topic: false,
-                    last_updated: "",
-                    lms_id: 0,
-                    name: "",
-                    parent_id: 0,
-                    student_topic: {done: false, done_at: "", id: 0, student_id: 0, topic_id: 0, visits: []},
-                    university: ""
-                }];
-                setTopics(emptyTopic);
-                setLoading(false); // set loading state to false if there is an error
-            }
-        };
-        fetchTopics();
-    }, []);
+        })};
 
-    return {loading, topics, learningPath};
-}
+    useEffect(() => {
+        effect();
+    },[]);
+
+    return { loading, topics, learningPath };
+};
