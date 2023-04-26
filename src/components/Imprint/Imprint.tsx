2d1c67d1640f37fae8903871185b05e657762287
DimitriB01 <d.bigler1996@gmx.de>
2023-04-26 14:44:00 +0200

    imprint inhalt

--- src/components/Imprint/Imprint.tsx
+++ src/components/Imprint/Imprint.tsx
@@ -1,8 +1,80 @@
-import {DefaultTypography} from "@common/components";
+import {DefaultTypography as Typography} from "@common/components";
+import { DefaultLink as Link } from "@common/components";
 export const Imprint = () => {
     return (
         <div>
-        <DefaultTypography variant='h4'>Imprint</DefaultTypography>
+            <Typography variant='h4'>Impressum</Typography>
+            <p></p>
+            <Typography variant='h6'>Lehr-/Lernplattform HASKI vom Labor Softwareentwicklung Hochschule Kempten</Typography>
+            <Typography variant='body2'>Stand 26.04.2023</Typography>
+            <p></p>
+            <Typography variant='body2'>Hochschule Kempten</Typography>
+            <Typography variant='body2'>Bahnhofstraße 61</Typography>
+            <Typography variant='body2'>D - 87435 Kempten</Typography>
+            <p></p>
+            <Typography variant='h6'>Vertretungsberechtigte Personen:</Typography>
+            <Typography variant='body2'>Herr Prof. Dr. Georg Hagel</Typography>
+            <Typography variant='body2'><Typography sx={{fontWeight:'bold', display: 'inline'}}>Tel.:</Typography> +49 (0) 831 2523-471</Typography>
+            <Typography variant='body2'><Typography sx={{fontWeight:'bold', display: 'inline'}}>E-Mail:</Typography> <Link href="mailto:georg.hagel@hs-kempten.de">georg.hagel@hs-kempten.de</Link></Typography>
+            <p></p>
+            <hr />
+            <Typography variant='h4'>Haftungsausschluss</Typography>
+            <p></p>
+            <Typography variant='h6'>1. Inhalt des Onlineangebotes</Typography>
+            <Typography variant='body2'>Die Hochschule Kempten übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit
+                oder Qualität der bereitgestellten Informationen. Haftungsansprüche gegen die Hochschule, welche sich auf Schäden
+                materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der dargebotenen Informationen bzw.
+                durch die Nutzung fehlerhafter und unvollständiger Informationen verursacht wurden, sind grundsätzlich ausgeschlossen,
+                sofern seitens der Hochschule kein nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt. Alle Angebote
+                sind freibleibend und unverbindlich. Die Hochschule Kempten behält es sich ausdrücklich vor, Teile der Seiten oder das
+                gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise
+                oder endgültig einzustellen.
+            </Typography>
+            <p></p>
+            <Typography variant='h6'>2. Verweise und Links</Typography>
+            <Typography variant='body2'>Bei direkten oder indirekten Verweisen auf fremde Webseiten (Hyperlinks), die außerhalb des
+                Verantwortungsbereiches der Hochschule liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall in Kraft treten,
+                in dem die Hochschule von den Inhalten Kenntnis hat und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle
+                rechtswidriger Inhalte zu verhindern. Die Hochschule Kempten erklärt hiermit ausdrücklich, dass zum Zeitpunkt der
+                Linksetzung keine illegalen Inhalte auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige
+                Gestaltung, die Inhalte oder die Urheberschaft der gelinkten/verknüpften Seiten hat die Hochschule keinerlei Einfluss.
+                Deshalb distanziert sie sich hiermit ausdrücklich von allen Inhalten aller gelinkten /verknüpften Seiten, die nach der
+                Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen Internetangebotes gesetzten Links und
+                Verweise . Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder
+                Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen
+                wurde, nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist.
+            </Typography>
+            <p></p>
+            <Typography variant='h6'>3. Urheber- und Leistungsschutzrechte</Typography>
+            <Typography variant='body2'>Die Hochschule Kempten ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten
+                Grafiken, Tondokumente, Videosequenzen und Texte zu beachten, von ihr selbst erstellte Grafiken, Tondokumente,
+                Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente, Videosequenzen und Texte zurückzugreifen.
+                Alle innerhalb des Internetangebotes genannten und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen
+                uneingeschränkt den Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen eingetragenen
+                Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen, dass Markenzeichen nicht durch Rechte
+                Dritter geschützt sind! Das Copyright für veröffentlichte, von der Hochschule erstellte Objekte bleibt allein bei der HS
+                Kempten. Eine Vervielfältigung oder Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in anderen
+                elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung der Fachhochschule nicht gestattet.
+            </Typography>
+            <p></p>
+            <Typography variant='h6'>4. Datenschutz</Typography>
+            <Typography variant='body2'>Sofern innerhalb des Internetangebotes die Möglichkeit zur Eingabe persönlicher oder geschäftlicher
+                Daten (Emailadressen, Namen, Anschriften) besteht, so erfolgt die Preisgabe dieser Daten seitens des Nutzers auf ausdrücklich
+                freiwilliger Basis. Die Inanspruchnahme aller angebotenen Dienste ist - soweit technisch möglich und zumutbar - auch ohne
+                Angabe solcher Daten bzw. unter Angabe anonymisierter Daten oder eines Pseudonyms gestattet. Die Nutzung der im Rahmen des
+                Impressums oder vergleichbarer Angaben veröffentlichten Kontaktdaten wie Postanschriften, Telefon- und Faxnummern sowie
+                Emailadressen durch Dritte zur Übersendung von nicht ausdrücklich angeforderten Informationen ist nicht gestattet.
+                Rechtliche Schritte gegen die Versender von sogenannten Spam-Mails bei Verstößen gegen dieses Verbot sind ausdrücklich
+                vorbehalten.
+            </Typography>
+            <p></p>
+            <Typography variant='h6'>5. Rechtswirksamkeit dieses Haftungsausschlusses</Typography>
+            <Typography variant='body2'>Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf diese
+                Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der geltenden Rechtslage nicht, nicht mehr
+                oder nicht vollständig entsprechen sollten, bleiben die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit
+                davon unberührt.
+            </Typography>
+            <p></p>
         </div>
     )
 }
