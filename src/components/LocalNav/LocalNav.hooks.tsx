0510bfc7aeac6caecd55d691915f90b05f87341f
DimitriB01 <d.bigler1996@gmx.de>
2023-05-04 10:03:37 +0200

    sonarCloud reported bugs, fixed

--- src/components/LocalNav/LocalNav.hooks.tsx
+++ src/components/LocalNav/LocalNav.hooks.tsx
@@ -1,45 +1,51 @@
-import { useEffect, useState } from 'react'
-import { getCourseTopics, getElementLearningPath, LearningPath, Topic } from '@services'
+import {useEffect, useState} from 'react'
+import {getCourseTopics, getElementLearningPath, LearningPath, Topic} from '@services'
 
-const getSortedLearningPath = async (data: Topic[]): Promise<LearningPath[]> => {
-  const promises = data.map((topic) => getElementLearningPath(topic.id))
-  const learningPaths = await Promise.all(promises)
+const getSortedLearningPath = async(data: Topic[]): Promise<LearningPath[]> => {
+    const promises = data.map((topic) => getElementLearningPath(topic.id))
+    const learningPaths = await Promise.all(promises)
 
-  return learningPaths
-    .filter((learningPath) => learningPath.status === 200)
-    .map((learningPath) => {
-      learningPath.data.path.sort((a, b) => a.position - b.position)
-      return learningPath.data
+    return learningPaths.filter((learningPath) => learningPath.status === 200).map((learningPath) => {
+        learningPath.data.path.sort((a, b) => a.position - b.position)
+        return learningPath.data
     })
 }
 
 export const useLearningPath = (): { loading: boolean; topics: Topic[]; learningPath: LearningPath[] } => {
-  const [loading, setLoading] = useState(true)
-  const [topics, setTopics] = useState<Topic[]>([])
-  const [learningPath, setLearningPath] = useState<LearningPath[]>([])
+    const [loading, setLoading] = useState(true)
+    const [topics, setTopics] = useState<Topic[]>([])
+    const [learningPath, setLearningPath] = useState<LearningPath[]>([])
 
-  const effect = async () => {
-    setLoading(true)
-    getCourseTopics().then((response) => {
-      if (response.status === 200) {
-        setTopics(response.data.topics)
+    const effect = async() => {
+        setLoading(true)
+        getCourseTopics().then((response) => {
+            if(response.status === 200) {
+                setTopics(response.data.topics)
 
-        getSortedLearningPath(response.data.topics).then((dataLearningPath) => {
-          setLearningPath(dataLearningPath)
-          setLoading(false)
+                getSortedLearningPath(response.data.topics).then((dataLearningPath) => {
+                    setLearningPath(dataLearningPath)
+                    setLoading(false)
+                }).catch(() => {
+                    setLoading(false)
+                    console.log("sortedLearningPath error")
+                })
+            }
+            else {
+                // some error occurred
+                setLoading(false)
+            }
+        }).catch(() => {
+            setLoading(false)
+            console.log("courseTopics error")
         })
-      } else {
-        // some error occurred
-        setLoading(false)
-      }
-    })
-  }
+    }
 
-  useEffect(() => {
-    effect().catch(() => {
-      setLoading(false)
-    })
-  }, [])
+    useEffect(() => {
+        effect().catch(() => {
+            setLoading(false)
+            console.log("useEffect error")
+        })
+    }, [])
 
-  return { loading, topics, learningPath }
+    return {loading, topics, learningPath}
 }
