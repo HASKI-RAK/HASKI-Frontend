f26db0ab1db3cb94ad714a88d1e8ce662098e625
David Fischer <davidvfischer@gmail.com>
2023-05-17 11:33:06 +0200

    fix

--- src/common/services/LearningPath/index.ts
+++
@@ -1,6 +0,0 @@
-import { getElementLearningPath } from './getElementLearningPath'
-import { getLearningPath } from './getLearningPath'
-import { LearningPath, PathItem, LearningElement, LearningPathRequestResponse } from './RequestResponse.d'
-
-export { getElementLearningPath, getLearningPath }
-export type { LearningPath, PathItem, LearningElement, LearningPathRequestResponse }
