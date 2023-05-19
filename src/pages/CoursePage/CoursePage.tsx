8f70d42bf2bf76206ccc5a4d25dd095bafd36fce
DimitriB01 <d.bigler1996@gmx.de>
2023-05-19 09:48:45 +0200

    added hardcoded topics

--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -6,9 +6,49 @@ import { useNavigate, useParams } from 'react-router-dom'
 // TODO: Replace this with the actual data from the backend
 const topics = [
   {
-    id: 2,
-    name: 'Topic 2',
+    id: 1,
+    name: 'General',
     description: 'This is the first topic'
+  },
+  {
+    id: 16,
+    name: 'Individuelle Anordnung',
+    description: 'This is the second topic'
+  },
+  {
+    id: 7,
+    name: 'Entwurfsmuster Allgemein',
+    description: 'This is the third topic'
+  },
+  {
+    id: 2,
+    name: 'Bekannte Entwurfsmuster',
+    description: 'This is the forth topic'
+  },
+  {
+    id: 3,
+    name: 'Strategie',
+    description: 'This is the fifth topic'
+  },
+  {
+    id: 4,
+    name: 'Zustand',
+    description: 'This is the sixth topic'
+  },
+  {
+    id: 5,
+    name: 'Adapter',
+    description: 'This is the seventh topic'
+  },
+  {
+    id: 6,
+    name: 'Fassade',
+    description: 'This is the eighth topic'
+  },
+  {
+    id: 14,
+    name: 'Abschluss',
+    description: 'This is the ninth topic'
   }
 ]
 
