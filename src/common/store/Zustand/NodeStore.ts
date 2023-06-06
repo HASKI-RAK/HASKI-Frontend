d34126f47236ac4faffae0cda9420722eac45aa4
David Fischer <davidvfischer@gmail.com>
2023-06-06 10:50:00 +0200

    persist to node store

--- src/common/store/Zustand/NodeStore.ts
+++ src/common/store/Zustand/NodeStore.ts
@@ -2,20 +2,25 @@ import create from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 export type Node = {
-    id: number
-    isOpen: boolean
+  id: number
+  isOpen: boolean
 }
 
 interface StoreState {
-    nodes: Node[]
-    getNode: (id: number) => any
-    setNode: (id: number, node: any) => void
+  nodes: Node[]
+  getNode: (id: number) => any
+  setNode: (id: number, node: any) => void
 }
-export const useBoundNodeStore = create<StoreState>()(devtools((set, get) => ({
-    nodes: [],
-    getNode: (id) => get().nodes.find((node) => node.id === id),
-    // add if not exists, update if exists
-    setNode: (id, node) => set({ nodes: [...get().nodes.filter((n) => n.id !== id), node] })
-
-}), { name: 'node-store' }))
-
+export const useBoundNodeStore = create<StoreState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        nodes: [],
+        getNode: (id) => get().nodes.find((node) => node.id === id),
+        // add if not exists, update if exists
+        setNode: (id, node) => set({ nodes: [...get().nodes.filter((n) => n.id !== id), node] })
+      }),
+      { name: 'node-store' }
+    )
+  )
+)
