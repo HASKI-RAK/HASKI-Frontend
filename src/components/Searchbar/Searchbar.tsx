cdc7d0adea9ce82396ac95d4340f76e6bf9a63d0
juli-txt <julian.manz@hs-kempten.de>
2023-06-26 11:35:27 +0200

    Searchbar comments

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -6,12 +6,25 @@ import {
   DefaultInputAdornment as InputAdornment
 } from '@common/components'
 
+/**
+ * @typedef {object} SearchbarProps
+ * @property {string} [label] - The label text of the searchbar.
+ * @property {function} [setSearchQuery] - The function to set the query that should be searched for.
+ * @property {number} [timeout] - The timeout in milliseconds to wait before searching.
+ */
 export type SearchbarProps = {
   label?: string
   setSearchQuery?: (query: string) => void
   timeout?: number
 }
 
+/**
+ * debouncedSearchQuery presents a hook that can be used to debounce a query by a timeout before it gets written into a state.
+ * debouncedSearchQuery can be used as a hook on in a component.
+ * @param props - Props containing a change event, a function to set the search query and a timeout.
+ * @returns {() => void} - The function thats clears the timeout.
+ * @category Hooks
+ */
 export const debouncedSearchQuery = (
   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   setSearchQuery?: (query: string) => void,
@@ -28,6 +41,14 @@ export const debouncedSearchQuery = (
   return () => clearTimeout(timer)
 }
 
+/**
+ * Searchbar presents a component that can be used to write a search query.
+ * The written search query gets debounced by a timeout before the value is set.
+ * Searchbar can be used as a standalone component on a page.
+ * @param props - Props containing the label text, the function to set the search query and a timeout of the searchbar.
+ * @returns {JSX.Element} - The Searchbar component.
+ * @category Components
+ */
 const Searchbar = (props: SearchbarProps) => {
   const handleChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
