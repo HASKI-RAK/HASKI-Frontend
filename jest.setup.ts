8189385cf2592132df508090bc96e7aca9352224
David Fischer <davidvfischer@gmail.com>
2023-07-03 19:57:51 +0200

    learning path and post in jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,6 @@
 // Jest setup file
 const original = {
   getUser: jest.fn().mockImplementation(() => {
-    console.log('mockgetUser')
     return Promise.resolve({
       id: 1,
       lmsUserId: 1,
@@ -15,6 +14,46 @@ const original = {
         userId: 1
       },
       university: 'string'
+    })
+  }),
+  getLearningPath: jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      id: 1,
+      course_id: 2,
+      based_on: 'string',
+      calculated_on: 'string',
+      path: [
+        {
+          id: 1,
+          learning_element_id: 1,
+          learning_path_id: 1,
+          recommended: true,
+          position: 1,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: 'KÜ',
+            classification: 'KÜ',
+            name: 'Kurzüberblick',
+            university: 'HS-Kempten',
+            created_at: 'string',
+            created_by: 'string',
+            last_updated: 'string',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: true,
+              done_at: 'string'
+            }
+          }
+        }
+      ]
+    })
+  }),
+  postLogin: jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      expiration: 999999999999999,
     })
   })
 }
@@ -60,70 +99,3 @@ jest.mock<typeof import("@services")>('@services', () => {
     }
   });
 });
-
-// ############################## Services ############################## //
-
-
-
-// const servic = prepareMock("./src/common/services/auth", "getUser", mockgetUser);
-// console.log(servic)
-
-
-
-// jest.mock<typeof import("@services")>('@services', () => {
-//     const services = jest.requireActual<typeof import("@services")>('@services');
-//     console.log('mocking services')
-//     return {
-//         __esModule: true,    //    <----- this __esModule: true is important
-//         ...jest.requireActual('@services'),
-//         getUser: mockgetUser
-//     };
-// })
-// jest.spyOn(services, 'getLogout').mockImplementation(() => {
-//     return Promise.resolve(undefined)
-// })
-
-// jest.spyOn(services, 'postLogin').mockImplementation(() => {
-//     return Promise.resolve({
-//         expiration: 999999999999999,
-//     })
-// })
-// // ############################## Log ############################## //
-
-// // ############################## LearningPath ############################## //
-// // SpyOn getLearningPath to return a mock learning path
-// jest.spyOn(services, 'getLearningPath').mockImplementation(() => {
-//     return Promise.resolve({
-//         id: 1,
-//         course_id: 2,
-//         based_on: 'string',
-//         calculated_on: 'string',
-//         path: [
-//             {
-//                 id: 1,
-//                 learning_element_id: 1,
-//                 learning_path_id: 1,
-//                 recommended: true,
-//                 position: 1,
-//                 learning_element: {
-//                     id: 1,
-//                     lms_id: 1,
-//                     activity_type: 'KÜ',
-//                     classification: 'KÜ',
-//                     name: 'Kurzüberblick',
-//                     university: 'HS-Kempten',
-//                     created_at: 'string',
-//                     created_by: 'string',
-//                     last_updated: 'string',
-//                     student_learning_element: {
-//                         id: 1,
-//                         student_id: 1,
-//                         learning_element_id: 1,
-//                         done: true,
-//                         done_at: 'string'
-//                     }
-//                 }
-//             }
-//         ]
-//     })
-// })
