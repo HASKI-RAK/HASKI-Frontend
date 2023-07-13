4da011f44c04abd3b6af513fe6d026d5814238eb
David Fischer <davidvfischer@gmail.com>
2023-07-13 12:59:00 +0200

    unified representation

--- src/common/services/LearningPath/getLearningPathElement.tsx
+++ src/common/services/LearningPath/getLearningPathElement.tsx
@@ -1,19 +1,37 @@
-import { LearningPathElement, LearningPathElementReturn } from '@core'
+import { LearningPathElement } from '@core'
 import { getData } from '../RequestResponse'
 
-export const getLearningPathElement: LearningPathElementReturn = async (
-  userId,
-  lmsUserId,
-  studentId,
-  course_id,
-  topic_id
-) => {
-  if (!course_id || !topic_id) {
+/**
+ * @typedef {Object} LearningPathTopicProps
+ * @property {number} userId
+ * @property {number} lmsUserId
+ * @property {number} studentId
+ * @property {string} courseId
+ * @property {string} topicId
+ */
+type LearningPathElementProps = {
+  userId?: number,
+  lmsUserId?: number,
+  studentId?: number,
+  courseId?: string,
+  topicId?: string
+}
+
+/**
+ * Returns the {@link LearningPathElement} for the given {@link LearningPathElementProps}
+ * @module
+ * @name getLearningPathElement
+ * @param {LearningPathElementProps} props 
+ * @returns {Promise<LearningPathElement>}
+ */
+export const getLearningPathElement = async (props: LearningPathElementProps) => {
+  const { userId, lmsUserId, studentId, courseId, topicId } = props
+  if (!courseId || !topicId) {
     throw new Error('course_id and topic_id are required')
   }
   const response = await fetch(
     process.env.BACKEND +
-      `/user/${userId}/${lmsUserId}/student/${studentId}/course/${course_id}/topic/${topic_id}/learningPath`,
+    `/user/${userId}/${lmsUserId}/student/${studentId}/course/${courseId}/topic/${topicId}/learningPath`,
     {
       method: 'GET',
       credentials: 'include',
