363733a9ebf41d45368547f4274430160b4f5771
David Fischer <davidvfischer@gmail.com>
2023-08-14 16:04:51 +0200

    typedoc updated github pages wiki

--- src/components/Nodes/index.ts
+++ src/components/Nodes/index.ts
@@ -1,2 +1,2 @@
 export { nodeTypes } from './NodeTypes/NodeTypes'
-export type { LearningPathLearningElementNode } from './NodeTypes/LearningPathLearningElementNode/LearningPathLearningElementNode'
+export type { LearningPathLearningElementNode } from './NodeTypes/NodeTypes'
