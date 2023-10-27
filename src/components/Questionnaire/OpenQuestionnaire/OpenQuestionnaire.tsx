1ea6e2627d53ce030b2077b468e06b169810e1f8
Gesine Wagner <80248709+Platura@users.noreply.github.com>
2023-10-27 13:30:24 +0200

    added cookies

--- src/components/Questionnaire/OpenQuestionnaire/OpenQuestionnaire.tsx
+++ src/components/Questionnaire/OpenQuestionnaire/OpenQuestionnaire.tsx
@@ -2,16 +2,34 @@ import { QuestionnaireQuestionsModal } from '@components'
 import { TableILSQuestions } from '../QuestionnaireQuestions/Table/TableILSQuestions'
 import { useTranslation } from 'react-i18next'
 import { useState } from 'react'
+import {
+  PrivacyModalHookReturn,
+  usePrivacyModal as _usePrivacyModal
+} from 'src/components/PrivacyModal/PrivacyModal.hooks'
+import { useCookies } from 'react-cookie'
 
-const OpenQuestionnaire = () => {
+export type PrivacyModalProps = {
+  usePrivacyModal?: () => PrivacyModalHookReturn
+}
+
+const OpenQuestionnaire = ({ usePrivacyModal = _usePrivacyModal }: PrivacyModalProps) => {
   const { t } = useTranslation()
-  const [modalOpenILSLong, setModalOpenILSShort] = useState(true)
+  const [modalOpenILSLong, setModalOpenILSLong] = useState(true)
   const [successSendILSLong, setSuccessSendILSLong] = useState(false)
-  const handleCloseILSShortModal = (event: object, reason: string) => {
-    if (reason == 'backdropClick') if (window.confirm(t('components.Menubar.CloseDialog'))) setModalOpenILSShort(false)
+  const [cookie, setCookie] = useCookies(['questionnaire_sent_token'])
+  const { privacyPolicyCookie } = usePrivacyModal()
+  const handleCloseILSLongModal = (event: object, reason: string) => {
+    if (reason == 'backdropClick')
+      if (window.confirm(t('components.Menubar.CloseDialog'))) {
+        setModalOpenILSLong(false)
+        setCookie('questionnaire_sent_token', true, { path: '/' })
+      }
+  }
+  if (!privacyPolicyCookie || cookie['questionnaire_sent_token']) {
+    return null
   }
   return (
-    <QuestionnaireQuestionsModal open={modalOpenILSLong} handleClose={handleCloseILSShortModal}>
+    <QuestionnaireQuestionsModal open={modalOpenILSLong} handleClose={handleCloseILSLongModal}>
       <TableILSQuestions ilsLong={true} successSend={successSendILSLong} setSuccessSend={setSuccessSendILSLong} />
     </QuestionnaireQuestionsModal>
   )
