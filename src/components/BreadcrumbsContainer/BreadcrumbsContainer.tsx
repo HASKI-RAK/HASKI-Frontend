300586b02c385eb9ec9502d45431994141c29e78
DimitriB01 <d.bigler1996@gmx.de>
2023-10-31 15:55:27 +0100

    breadcrumbs do not show ids anymore, when clicking on course or topic, it will redirect to the correct course or topic. As the user expects

--- src/components/BreadcrumbsContainer/BreadcrumbsContainer.tsx
+++ src/components/BreadcrumbsContainer/BreadcrumbsContainer.tsx
@@ -1,6 +1,6 @@
-import { Box, Breadcrumbs, Link, Typography } from '@common/components'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
+import {Box, Breadcrumbs, Link, Typography} from '@common/components'
+import {useLocation, useNavigate} from 'react-router-dom'
+import {useTranslation} from 'react-i18next'
 
 /**
  *  The BreadcrumbsContainer that renders breadcrumbs.
@@ -13,65 +13,72 @@ import { useTranslation } from 'react-i18next'
  * @category Components
  */
 const BreadcrumbsContainer = () => {
-  // UX Logic
-  const { t } = useTranslation()
-  const navigate = useNavigate()
-  const location = useLocation()
+    // UX Logic
+    const {t} = useTranslation()
+    const navigate = useNavigate()
+    const location = useLocation()
+    const regex = /[0-9]/g
 
-  return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      {/** Center */}
-      <Breadcrumbs aria-label="breadcrumb">
-        {location.pathname !== '/' ? (
-          location.pathname.split('/').map((path, index) => {
-            if (path === '')
-              return (
-                <Link
-                  key={path}
-                  underline="hover"
-                  color="text.primary"
-                  onClick={() => {
-                    navigate('/')
-                  }}>
-                  {t('pages.home')}
-                </Link>
-              )
+    return (
+        <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            {/** Center */}
+            <Breadcrumbs aria-label="breadcrumb">
+                {location.pathname !== '/' ? (
+                    location.pathname.split('/').map((path, index, array) => {
+                        if(path === '')
+                            return (
+                                <Link
+                                    key={path}
+                                    underline="hover"
+                                    color="text.primary"
+                                    onClick={() => {
+                                        navigate('/')
+                                    }}>
+                                    {t('pages.home')}
+                                </Link>
+                            )
 
-            return (
-              <Link
-                key={path}
-                underline="hover"
-                component={index === location.pathname.split('/').length - 1 ? 'span' : 'button'}
-                color={index === location.pathname.split('/').length - 1 ? 'text.primary' : 'inherit'}
-                onClick={() => {
-                  navigate(
-                    location.pathname
-                      .split('/')
-                      .slice(0, index + 1)
-                      .join('/')
-                  )
-                }}>
-                {t(`pages.${path}`)}
-              </Link>
-            )
-          })
-        ) : (
-          <Box display="flex">
-            <Link
-              color="text.primary"
-              onClick={() => {
-                navigate('/')
-              }}>
-              {t('pages.home')}
-            </Link>
-            <Typography ml="0.3rem" color="text.primary">
-              /
-            </Typography>
-          </Box>
-        )}
-      </Breadcrumbs>
-    </Box>
-  )
+                        //do not display numbers in breadcrumbs
+                        if(regex.test(array[index+1]))
+                        {
+                            return (
+                                ""
+                            )
+                        }
+                        else{
+                            return (
+                                <Link
+                                    key={path}
+                                    underline="hover"
+                                    component={index === location.pathname.split('/').length - 1 ? 'span' : 'button'}
+                                    color={index === location.pathname.split('/').length - 1 ? 'text.primary' : 'inherit'}
+                                    onClick={() => {
+                                        navigate(
+                                            location.pathname.split('/').slice(0, index + 1).join('/')
+                                        )
+                                    }}>
+                                    {regex.test(array[index]) ? t(`pages.${array[index-1].replace(regex, '').replaceAll("/","")}`) : t(`pages.${path}`)}
+                                </Link>
+                            )
+                        }
+                    })
+                ) : (
+                    <Box display="flex">
+                        <Link
+                            color="text.primary"
+                            onClick={() => {
+                                navigate('/')
+                            }}>
+                            {t('pages.home')}
+                        </Link>
+                        <Typography ml="0.3rem" color="text.primary">
+                            /
+                        </Typography>
+                    </Box>
+                )}
+            </Breadcrumbs>
+        </Box>
+    )
 }
 
 export default BreadcrumbsContainer
