1ed9f4caff7604ae9b8268469c3070880bca3db6
juli-txt <julian.manz@hs-kempten.de>
2023-11-08 17:36:55 +0100

    Added lrs url and credential to env file Fixed IFrameModal tests Added comment to xAPI.setup

--- src/services/xAPI/xAPI.setup.ts
+++ src/services/xAPI/xAPI.setup.ts
@@ -1,16 +1,22 @@
 import XAPI from '@xapi/xapi'
+import { getConfig } from '@shared'
 
-// Create LRS connection
-// endpoint: endpoint url
-// auth: string with basic auth credentials
-// version: used xAPI version
 /**
- * getConfig in config.ts
- * variablen in public/config
+ * xAPI object.
+ *
+ * @prop endpoint - URL to LRS endpoint
+ * @prop auth - Credentials to login to LRS
+ * @prop version - xAPI version
+ *
+ * @remarks
+ * xAPI object that is used to send statements to an LRS with the URL and credentials of the endpoint.
+ * Adds xAPI version to the header of an xAPI statement.
+ *
+ * @category Services
  */
 const xAPI: XAPI = new XAPI({
-  endpoint: 'http://127.0.0.1:5000/endpoint', //'https://dev.lrs.haski.app/xapi', //
-  auth: 'Basic MmUzYzEwMGJlNDRkNDI0MGM5MmJhN2Y3NzY4YzMwMDllNzU0MTBlZjJjNTQ4YzNkYWJlZjY0ZTlmYTZhODIzNjphNjFiYjQwYTAzMzMyNWM4NTZmYzcxNzZmZmRjZGE5MjgyZGQxYzcwMWIzOWM2ZDc3NGY2NTA3YzNjNjFhNzJm', // XAPI.toBasicAuth('test', 'test'),
+  endpoint: getConfig().LRS ?? '',
+  auth: getConfig().LRS_AUTH,
   version: '1.0.3'
 })
 
