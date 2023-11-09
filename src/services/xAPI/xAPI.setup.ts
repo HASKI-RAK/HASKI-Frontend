a82d4a73b393e5738148381b92322929ee01dfb9
juli-txt <julian.manz@hs-kempten.de>
2023-11-09 13:36:39 +0100

    Changes to meet requests

--- src/services/xAPI/xAPI.setup.ts
+++ src/services/xAPI/xAPI.setup.ts
@@ -1,5 +1,4 @@
 import XAPI from '@xapi/xapi'
-import { getConfig } from '@shared'
 
 /**
  * xAPI object.
@@ -15,7 +14,7 @@ import { getConfig } from '@shared'
  * @category Services
  */
 const xAPI: XAPI = new XAPI({
-  endpoint: getConfig().LRS ?? '', // 'http://127.0.0.1:5000/endpoint', // 'https://dev.lrs.haski.app/xapi', //
+  endpoint: 'https://dev.lrs.haski.app/xapi',
   auth: 'Basic MmUzYzEwMGJlNDRkNDI0MGM5MmJhN2Y3NzY4YzMwMDllNzU0MTBlZjJjNTQ4YzNkYWJlZjY0ZTlmYTZhODIzNjphNjFiYjQwYTAzMzMyNWM4NTZmYzcxNzZmZmRjZGE5MjgyZGQxYzcwMWIzOWM2ZDc3NGY2NTA3YzNjNjFhNzJm', // getConfig().LRS_AUTH,
   version: '1.0.3'
 })
