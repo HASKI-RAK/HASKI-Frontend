37bc895fb8f5d3981a616977568cac89f779fbcc
DimitriB01 <d.bigler1996@gmx.de>
2023-11-22 10:05:29 +0100

    first example try

--- xapi/verbs/answered.ts
+++ xapi/verbs/answered.ts
@@ -3,9 +3,14 @@
  * # answered
  * @remarks
  * The "answered" verb is used to indicate that the user answered something.
+ * The user answered a questions, h5p-questions, etc.
  *
  * @example
- * The user answered a questions, h5p-questions, etc.
+ *  "verb": {
+ *     "id": "https://wiki.haski.app/variables/xapi.answered", // URI of action in online directory
+ *     "display": {
+ *       "en": "submitted" // en required
+ *     }
  *
  * @category verbs
  */
