1ff428475f2944f3f77ed91cd71126d1a590b395
DimitriB01 <d.bigler1996@gmx.de>
2023-11-27 12:26:13 +0100

    linted

--- src/services/xAPI/index.ts
+++ src/services/xAPI/index.ts
@@ -1,10 +1,9 @@
-
 export { default as xAPI } from './xAPI.setup'
 export * from './Statement.hooks'
 export * from './getStatement'
 
-export {default as answered} from './verbs/answered'
-export {default as clicked} from './verbs/clicked'
-export {default as pressed} from './verbs/pressed'
-export {default as selected} from './verbs/selected'
-export {default as started} from './verbs/started'
+export { default as answered } from './verbs/answered'
+export { default as clicked } from './verbs/clicked'
+export { default as pressed } from './verbs/pressed'
+export { default as selected } from './verbs/selected'
+export { default as started } from './verbs/started'
