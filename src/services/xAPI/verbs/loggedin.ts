e960e1a3820531839de7694fd57f7fe26b3a0c2f
DimitriB01 <d.bigler1996@gmx.de>
2023-12-05 09:34:42 +0100

    added more verbs for moodle xapi-calls

---
+++ src/services/xAPI/verbs/loggedin.ts
@@ -0,0 +1,18 @@
+/**
+ * # loggedin
+ * @remarks
+ * The "loggedin" verb is used to indicate that the user loggedin somewhere,
+ * most likely the system itself.
+ *
+ * @example
+ *  "verb": {
+ *     "id": "https://wiki.haski.app/variables/xapi.loggedin",
+ *     "display": {
+ *       "en": "loggedin"
+ *     }
+ *  },
+ *
+ * @category verbs
+ */
+
+export default 'verbs/loggedin'
