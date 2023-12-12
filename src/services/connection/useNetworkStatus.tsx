fa028a2c6d9acfe753a612e2613c23032992c92e
juli-txt <julian.manz@hs-kempten.de>
2023-12-12 09:45:42 +0100

    Updated test and documentation of useNetworkStatus

--- src/services/connection/useNetworkStatus.tsx
+++ src/services/connection/useNetworkStatus.tsx
@@ -1,19 +1,39 @@
 import { useState, useEffect, useMemo } from 'react'
 
 /**
- * Service hook for the network status. Returns true if the user is online
- * and false otherwise.
- * @returns {boolean} - The network status.
+ * @prop online - The default value for the network status.
+ * @category Services
+ * @category Hooks
+ * @interface
  */
-
 export type useNetworkStatusHookParams = {
   online?: boolean
 }
 
+/**
+ * @prop isOnline - The current network status.
+ * @category Services
+ * @category Hooks
+ * @interface
+ */
 export type NetworkStatusHookReturn = {
   readonly isOnline: boolean
 }
 
+/**
+ * useNetworkStatus hook.
+ *
+ * @param params - The default values for the network status.
+ *
+ * @remarks
+ * Service hook for the network status. Returns true if the user is online and false otherwise.
+ *
+ * @returns
+ * The current network status.
+ *
+ * @category Services
+ * @category Hooks
+ */
 export const useNetworkStatus = (params?: useNetworkStatusHookParams): NetworkStatusHookReturn => {
   // Default values.
   const { online = navigator.onLine } = params ?? {}
