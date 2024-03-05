80f8ba0d710ece2194752b5bf7e8905a11509b84
Kevin Lee <KevinKimLee95@gmail.com>
2024-03-05 10:31:21 +0100

    removed unused imports

--- src/components/WrappedMiniMap/WrappedMap.test.tsx
+++ src/components/WrappedMiniMap/WrappedMap.test.tsx
@@ -1,16 +1,24 @@
 import '@testing-library/jest-dom'
-import { act, fireEvent, render, renderHook, waitFor, screen, getByTestId } from '@testing-library/react'
+import { act, fireEvent, render } from '@testing-library/react'
 import { ReactFlowProvider } from 'reactflow'
 import WrappedMiniMap from './WrappedMiniMap'
 
 describe('WrappedMiniMap component', () => {
   it('renders', () => {
-    const {getByTestId} = render(<ReactFlowProvider><WrappedMiniMap /></ReactFlowProvider>)
+    const { getByTestId } = render(
+      <ReactFlowProvider>
+        <WrappedMiniMap />
+      </ReactFlowProvider>
+    )
     expect(getByTestId('rf__minimap')).toBeInTheDocument()
   })
 
   it('resizes according to window size', () => {
-    const {getByTestId} = render(<ReactFlowProvider><WrappedMiniMap /></ReactFlowProvider>)
+    const { getByTestId } = render(
+      <ReactFlowProvider>
+        <WrappedMiniMap />
+      </ReactFlowProvider>
+    )
     expect(getByTestId('rf__minimap')).toHaveStyle('transform: scale(0.75) translate(1.5rem, 1.5rem)')
     act(() => {
       global.innerWidth = 1921
@@ -25,14 +33,14 @@ describe('WrappedMiniMap component', () => {
     expect(getByTestId('rf__minimap')).toHaveStyle('transform: scale(1.2) translate(-1rem, -1rem)')
 
     act(() => {
-        global.innerWidth = 1300
-        fireEvent(window, new Event('resize'))
+      global.innerWidth = 1300
+      fireEvent(window, new Event('resize'))
     })
     expect(getByTestId('rf__minimap')).toHaveStyle('transform: scale(1) translate(0rem, 0rem)')
 
     act(() => {
-        global.innerWidth = 700
-        fireEvent(window, new Event('resize'))
+      global.innerWidth = 700
+      fireEvent(window, new Event('resize'))
     })
     expect(getByTestId('rf__minimap')).toHaveStyle('transform: scale(0.5) translate(6rem, 5rem)')
   })
--- src/components/WrappedMiniMap/WrappedMiniMap.tsx
+++ src/components/WrappedMiniMap/WrappedMiniMap.tsx
@@ -1,49 +1,53 @@
 import { MiniMap } from 'reactflow'
 import { MiniMapProps } from '@reactflow/minimap/dist/esm/types'
 import { useTheme } from '@common/hooks'
-import { useEffect, useState, useContext, memo } from 'react'
+import { useEffect, useState, memo } from 'react'
 
+const WrappedMiniMap: React.FC<MiniMapProps> = () => {
+  const theme = useTheme()
 
-const WrappedMiniMap: React.FC<MiniMapProps> = (props) => {
-    const theme = useTheme()
-
-    const getMapTransform = () => {
-        if(window.innerWidth > 1920) {
-          return {transform: 'scale(1.5) translate(-1.5rem, -1.5rem)'}
-        }
-        if(window.innerWidth > theme.breakpoints.values.xl) {
-          return {transform: 'scale(1.2) translate(-1rem, -1rem)'}
-        }
-        if(window.innerWidth > theme.breakpoints.values.lg) {
-          return {transform: 'scale(1) translate(0rem, 0rem)'}
-        }
-        if(window.innerWidth > theme.breakpoints.values.md) {
-          return {transform: 'scale(0.75) translate(1.5rem, 1.5rem)'}
-        }
-        else {
-          return {transform: 'scale(0.5) translate(6rem, 5rem)'}
-        }
+  const getMapTransform = () => {
+    if (window.innerWidth > 1920) {
+      return { transform: 'scale(1.5) translate(-1.5rem, -1.5rem)' }
     }
+    if (window.innerWidth > theme.breakpoints.values.xl) {
+      return { transform: 'scale(1.2) translate(-1rem, -1rem)' }
+    }
+    if (window.innerWidth > theme.breakpoints.values.lg) {
+      return { transform: 'scale(1) translate(0rem, 0rem)' }
+    }
+    if (window.innerWidth > theme.breakpoints.values.md) {
+      return { transform: 'scale(0.75) translate(1.5rem, 1.5rem)' }
+    } else {
+      return { transform: 'scale(0.5) translate(6rem, 5rem)' }
+    }
+  }
 
-    const [minimapSize, setMinimapSize] = useState(getMapTransform())
+  const [minimapSize, setMinimapSize] = useState(getMapTransform())
 
-    const handleResize = () => {
-        setMinimapSize(getMapTransform())
-      }
-      
-      useEffect(() => {
-        window.addEventListener('resize', handleResize)
-        return () => {
-          window.removeEventListener('resize', handleResize)
-        }
-      }, [handleResize])
+  const handleResize = () => {
+    setMinimapSize(getMapTransform())
+  }
 
-    return <MiniMap style={minimapSize} nodeBorderRadius={2} 
-    nodeColor={theme.palette.primary.light}
-    maskStrokeColor='#000000' 
-    maskStrokeWidth={2} nodeStrokeColor={'#f57f17'} 
-    nodeStrokeWidth={5}
-    data-testid="wrapped-minimap"/>
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [handleResize])
+
+  return (
+    <MiniMap
+      style={minimapSize}
+      nodeBorderRadius={2}
+      nodeColor={theme.palette.primary.light}
+      maskStrokeColor="#000000"
+      maskStrokeWidth={2}
+      nodeStrokeColor={'#f57f17'}
+      nodeStrokeWidth={5}
+      data-testid="wrapped-minimap"
+    />
+  )
 }
 
 export default memo(WrappedMiniMap)
