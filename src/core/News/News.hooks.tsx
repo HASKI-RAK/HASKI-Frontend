98ecad3057a6a820e30da3c870a09904418244e0
Gesine Wagner <80248709+Platura@users.noreply.github.com>
2024-04-10 12:18:42 +0200

    added a checkNews to the hooks

--- src/core/News/News.hooks.tsx
+++ src/core/News/News.hooks.tsx
@@ -1,10 +1,29 @@
-import {fetchNews}from'@services'
+import log from 'loglevel'
+import { useMemo } from 'react'
+import { usePersistedStore } from "@store"
 
 export type NewsbannerHookReturn = {
-    readonly handleClose:()=> void
+    //readonly handleClose:()=> void
+    readonly checkNews:()=>Promise<string>
 }
 
 export const useNewsbanner = ():NewsbannerHookReturn=> {
-    const fetchNews = usw.
-    return useMemo()
+    const getNews = usePersistedStore((state)=>state.getNews)
+
+    //** Logic **/
+    //get the content of the news
+    const checkNews = async()=>{
+        return getNews()
+        .then((news)=>{
+            return news.news_content
+        })
+        .catch((reason)=>{
+            log.error(reason)
+            return ''
+        })
+    }
+    return useMemo(
+        () =>({checkNews}),
+        [checkNews]
+    )
 }
