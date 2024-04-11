509ffdde2d6434de40a5463601a1c4ff36894a8e
Gesine Wagner <80248709+Platura@users.noreply.github.com>
2024-04-11 12:38:51 +0200

    removed scrollbar

--- src/components/Newsbanner/Newsbanner.tsx
+++ src/components/Newsbanner/Newsbanner.tsx
@@ -1,11 +1,7 @@
-import { Alert, Box, Collapse, IconButton, makeStyles } from "@mui/material";
+import { Alert, Box, Collapse, IconButton } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import { useState } from "react";
-import {css, keyframes} from "@emotion/react"
-
-const animatedItem = css `{
-        animation: $scrolling 3s linear infinite
-    }`;
+import { keyframes} from "@emotion/react"
 
 const scrolling = keyframes`
     from {
@@ -19,17 +15,22 @@ const scrolling = keyframes`
 const Newsbanner=()=>{
     const [open, setOpen]=useState(true)
     //fetch von backend, localStorage abfrage ob schon gesehen, date, expirationdate, 
+    //TODO: Text anpassen auf Bildschrimgroesse und textlaenge
     return(
         <Box sx={{width:'100%'}}>
             
 
-            <Collapse in={open}>
-                <Alert severity="info"  action={<IconButton aria-label="close" sx={{overflow:'auto'}} color="inherit" size="small" onClick={()=>{setOpen(false)}}>
+            <Collapse in={open} sx={{overflow:'hidden'}}>
+                <Alert severity="info" sx={{ [`& .MuiAlert-message`]: {
+          overflow:'hidden'
+        }}}
+                action={<IconButton aria-label="close" color="inherit" size="small" onClick={()=>{setOpen(false)}}>
                 <CloseIcon fontSize="inherit" />
                 </IconButton>
                 }
                 >
-                    <Box sx={{overflow:'hidden', animation: `${scrolling} 30s linear infinite`, transform: `translateX(100%)`, width: `1600px` }} >
+                    <Box sx={{ animation: `${scrolling} 30s linear infinite`, transform: `translateX(100%)`, width: `1600px` }} >
+                   
                     Due to unforseen circumstances we regret to inform you that aliens have taken over humanity and we are doomed on another note the animalpark munich just had another cute lion baby 
                     </Box>
                 </Alert>
