e1d9fe8aaaf3be7ba6c0b3dc73462951312735b0
Kevin Lee <KevinKimLee95@gmail.com>
2024-05-02 13:37:25 +0200

    added save button and display components more dynamically

--- src/components/AlgorithmSettingsModal/AlgorithmSettingsModal.tsx
+++ src/components/AlgorithmSettingsModal/AlgorithmSettingsModal.tsx
@@ -1,6 +1,5 @@
 import { memo, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Form, useLocation, useNavigate } from 'react-router-dom'
 import {
   FormControlLabel,
   Grid,
@@ -8,26 +7,33 @@ import {
   RadioGroup,
   IconButton,
   Modal,
+  Button,
   Tooltip,
   Typography,
   Radio,
-  Divider
+  Divider,
+  Fab
 } from '@common/components'
+import { Close, Save } from '@common/icons'
+import { useTheme, useMediaQuery } from '@common/hooks'
 
 const AlgorithmSettingsModal = () => {
     const [open, setOpen] = useState(true)
     const [selected, setSelected] = useState(0)
     const { t } = useTranslation()
-    const options = t('components.AlgorithmSettingsModal.algorithmNames', {returnObjects: true}) as string[];
-    const descriptions = t('components.AlgorithmSettingsModal.algorithmDescriptions', {returnObjects: true}) as string[];
+    const options = t('components.AlgorithmSettingsModal.algorithms', {returnObjects: true}) as [{name: string, description: string, key: string}]
     const handleSelect = useCallback((event: React.ChangeEvent<HTMLInputElement>) => { setSelected(parseInt(event.target.value)) }, [setSelected])
+    const handleSave = useCallback(() => {
+      // TODO: Set the selected algorithm for the Course 
+    setOpen(false) }, [setOpen])
+    const theme = useTheme()
 
     return <Modal
       open={open}
       onClose={() => setOpen(false)}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description">
-        <Grid sx={{width: 800, height: 600, right: 50, bgcolor: 'background.paper', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+        <Grid sx={{width: {xl: '50rem', lg: '40rem', md: '30rem', xs:'18rem'},height: '30rem', right: 50, bgcolor: 'background.paper', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
          border: '2px solid #D4D4D4',
          borderRadius: 6,
          boxShadow: 24,
@@ -35,20 +41,27 @@ const AlgorithmSettingsModal = () => {
         display: 'flex'}}>
             <Stack direction='row' spacing={2}>
               <Stack direction='column' spacing={2}>
-                <Typography id="modal-title" variant="h6" component="h6" align='center'> {t('components.AlgorithmSettingsModal.headerLeft')} </Typography>
+                <Typography id="modal-title" variant='h6' component="h6" align='center'> {t('components.AlgorithmSettingsModal.headerLeft')} </Typography>
                 <RadioGroup onChange={handleSelect}>
                   {options.map((option, index) => {
-                    return <FormControlLabel value={index} control={<Radio />} label={option} key={index} />
+                    return <FormControlLabel sx={{width: {xl: '16rem'}}}  value={index} control={<Radio checked={index === selected}/>} label={option.name} key={option.key} />
                   })}
                 </RadioGroup>
               </Stack>
-              <Divider orientation='vertical'/>
-                <Stack direction='column' spacing={2}>
-                <Typography id="modal-description-header" variant="h6" component="p" align='center'> {t('components.AlgorithmSettingsModal.headerRight')} </Typography>
-                <Typography id='modal-description' variant="body1" component="p" align='center'> {descriptions[selected]} </Typography>
-                <IconButton onClick={() => setOpen(false)} aria-label="close" sx={{position: 'absolute', right: 8, top: 8}}></IconButton>
-              </Stack>
+              <Fab sx={{width: {xl: '3.5rem', md:'2rem', sm: '2rem', xs: '2rem'}, height:{xl: '3.5rem', md:'2rem', xs: '2rem'}, position: 'absolute', top: '0.5rem', right:'0.5rem'}} color="primary" onClick={() => setOpen(false)}><Close/></Fab>
+              {useMediaQuery(theme.breakpoints.up('md')) && 
+                <>
+                  <Divider orientation='vertical'/>
+                  <Stack direction='column' spacing={2}>
+                    <Typography id="modal-description-header" variant="h6" component="p" align='center'> {t('components.AlgorithmSettingsModal.headerRight')} </Typography>
+                    <Typography id='modal-description' variant="body1" component="p" align='center'> {options[selected].description} </Typography>
+                  </Stack>  
+                </>
+              }
             </Stack>
+            <Button onClick={handleSave} aria-label="save" sx={{position: 'absolute', width: '2rem', height: '1.5rem', left: '85%', top: '85%'}}>
+              <Save/>
+            </Button>    
         </Grid>
       </Modal>
 }
