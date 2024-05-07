831b3e9ae229d6b35afcc63716d4bec50d2ae7bd
juli-txt <julian.manz@hs-kempten.de>
2024-05-07 16:00:56 +0200

    wtf

--- src/common/components/DefaultAccordion/DefaultAccordion.test.tsx
+++ src/common/components/DefaultAccordion/DefaultAccordion.test.tsx
@@ -2,13 +2,15 @@ import '@testing-library/jest-dom'
 import { act, fireEvent, render, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { AccordionSummary } from '@common/components'
-import { AuthContext, xAPI } from '@services'
+import { AuthContext } from '@services'
+import { PersistedStoreState, usePersistedStore } from '@store'
 import Accordion from './DefaultAccordion'
 
 describe('DefaultAccordion tests', () => {
   test('DefaultAccordion sends statement on click', async () => {
+    const xAPI = usePersistedStore((state) => state.getXAPI)()
     const sendStatement = jest.fn()
-    jest.spyOn(xAPI, 'sendStatement').mockImplementation(sendStatement)
+    jest.spyOn(xAPI!, 'sendStatement').mockImplementation(sendStatement)
 
     const { getByRole } = render(
       <AuthContext.Provider value={{ isAuth: true, setExpire: jest.fn(), logout: jest.fn() }}>
