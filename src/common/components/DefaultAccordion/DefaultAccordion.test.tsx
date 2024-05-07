a77eefa02dc73c419aa987b96dd7ceb69fc1c0fd
juli-txt <julian.manz@hs-kempten.de>
2024-05-07 16:53:03 +0200

    Fixed all default tests

--- src/common/components/DefaultAccordion/DefaultAccordion.test.tsx
+++ src/common/components/DefaultAccordion/DefaultAccordion.test.tsx
@@ -3,14 +3,24 @@ import { act, fireEvent, render, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { AccordionSummary } from '@common/components'
 import { AuthContext } from '@services'
-import { PersistedStoreState, usePersistedStore } from '@store'
 import Accordion from './DefaultAccordion'
 
 describe('DefaultAccordion tests', () => {
   test('DefaultAccordion sends statement on click', async () => {
-    const xAPI = usePersistedStore((state) => state.getXAPI)()
-    const sendStatement = jest.fn()
-    jest.spyOn(xAPI!, 'sendStatement').mockImplementation(sendStatement)
+    const usePersistedStore = jest.fn().mockReturnValue({
+      state: {
+        getXAPI: () => ({
+          sendStatement: jest.fn()
+        })
+      }
+    })
+
+    jest.mock('@store', () => ({
+      ...jest.requireActual('@store'),
+      usePersistedStore: () => usePersistedStore
+    }))
+
+    const sendStatement = usePersistedStore().state.getXAPI.sendStatement
 
     const { getByRole } = render(
       <AuthContext.Provider value={{ isAuth: true, setExpire: jest.fn(), logout: jest.fn() }}>
