6b5ae6b8fbe6c4029732e8c69070814f3261ceac
Gesine Wagner <80248709+Platura@users.noreply.github.com>
2024-05-21 13:19:59 +0200

    added snackbar and errormessage

--- src/components/Newsbanner/Newsbanner.hooks.tsx
+++ src/components/Newsbanner/Newsbanner.hooks.tsx
@@ -1,7 +1,9 @@
 import log from 'loglevel'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState, useContext } from 'react'
 import { UniversityCheck as _UniversityCheck, UniversityCheck, UniversityCheckReturn } from '@common/utils'
+import { SnackbarContext } from '@services'
 import { useStore } from '@store'
+import { useTranslation } from 'react-i18next'
 import i18next from 'i18next'
 
 export type NewsbannerHookReturn = {
@@ -10,17 +12,17 @@ export type NewsbannerHookReturn = {
 }
 
 export const useNewsbanner = (): NewsbannerHookReturn => {
+  const {t}=useTranslation()
+  const { addSnackbar } = useContext(SnackbarContext)
   const getNews = useStore((state) => state.getNews)
   const { checkUniversity } = UniversityCheck()
   const [lang, setLang]=useState(localStorage.getItem('i18nextLng')?.toLowerCase())
   const [newsItem, setNewsItem] = useState(false)
-  const [toggle, setToggle] = useState(false)
+  
 
   i18next.on('languageChanged', (lng: string) => {
     console.log('Language changed to:', lng);
     setLang(lng)
-    // Translate and log the new language
-    console.log(i18next.t('key'));
   })
   //** Logic **/
   const checkLanguage = () => {
@@ -32,6 +34,7 @@ export const useNewsbanner = (): NewsbannerHookReturn => {
   }
 
   //returns combined string of all the news
+  //and the amount of messages
   const checkForNews = async () => {
     return checkUniversity()
       .then((university) => {
@@ -42,13 +45,23 @@ export const useNewsbanner = (): NewsbannerHookReturn => {
             console.log('news:', news, 'lang', checkLanguage(), 'university:', university)
             return JSON.stringify(contentA)
           })
-          .catch((reason) => {
-            log.error(reason)
+          .catch((error) => {
+            addSnackbar({
+              message: t('error.getNews') + error,
+              severity: 'error',
+              autoHideDuration: 3000
+            })
+            log.error('error.getNews', 'Error: '+ error)
             return ''
           })
       })
-      .catch((reason) => {
-        log.error(reason)
+      .catch((error) => {
+        addSnackbar({
+          message: t('error.getUniversity') + error,
+          severity: 'error',
+          autoHideDuration: 3000
+        })
+        log.error('error.getUniversity', 'Error: '+ error)
         return ''
       })
   }
