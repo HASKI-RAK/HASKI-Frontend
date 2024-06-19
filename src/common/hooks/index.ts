557e32fd95bbcc8d45e855306387c0988076a1b8
DimitriB01 <d.bigler1996@gmx.de>
2024-06-19 09:26:40 +0200

    deleted unused LearningPathTopic.hooks export

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useTheme } from './DefaultUseTheme/DefaultUseTheme'
 export { useMediaQuery } from './DefaultMediaQuery/DefaultMediaQuery'
-export { useLearningPathTopic } from './LearningPathTopic/LearningPathTopic.hooks'
 export { useLearningPathTopicProgress } from './LearningPathTopicProgress/LearningPathTopicProgress.hooks'
