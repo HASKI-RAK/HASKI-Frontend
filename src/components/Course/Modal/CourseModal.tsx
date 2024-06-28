e8e2535976bffcda214aa745cac354f48d21704b
DimitriB01 <d.bigler1996@gmx.de>
2024-06-28 08:59:23 +0200

    choose algorithm like kevin

--- src/components/Course/Modal/CourseModal.tsx
+++ src/components/Course/Modal/CourseModal.tsx
@@ -4,17 +4,18 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import {
   Box,
   Button,
-  Checkbox, Fab,
+  Checkbox,
+  Fab,
   FormControlLabel,
   FormGroup,
+  Grid,
   Link,
   Modal,
   Tooltip,
-  Grid,
   Typography
 } from '@common/components'
-import { RemoteCourse } from '@core'
 import { Close } from '@common/icons'
+import { RemoteCourse } from '@core'
 import TableCourse from '../../Course/Table/TableCourse'
 import TableCourseDetails from '../Table/TableCourseDetails'
 
@@ -24,89 +25,79 @@ type CourseModalProps = {
 }
 
 const CourseModal = memo(({ open = false, handleClose }: CourseModalProps) => {
-    const { t } = useTranslation()
-    const navigate = useNavigate()
-    const [selectedCourse, setSelectedCourse] = useState<RemoteCourse>()
-    const [activeStep, setActiveStep] = useState<number>(0)
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+  const [selectedCourse, setSelectedCourse] = useState<RemoteCourse>()
+  const [activeStep, setActiveStep] = useState<number>(0)
 
-    const handleCourseSelection = (course: RemoteCourse) => {
-      setSelectedCourse(course)
-    }
-
-    return (
-      <Modal open={open} onClose={handleClose}>
-        <Grid container justifyContent="center" alignItems="center">
-          <Box
-            sx={{
-              position: 'absolute',
-              left: '20%',
-              right: '20%',
-              top: '10%',
-              overflow: 'auto',
-              maxHeight: '83%',
-              bgcolor: 'background.paper',
-              border: (theme) => '2px solid' + theme.palette.secondary.dark,
-              boxShadow: 24,
-              p: 1
-            }}>
-            <Fab
-              color="primary"
-              data-testid={'QuestionnaireResultsCloseButton'}
-              onClick={handleClose}
-              sx={{
-                position: 'sticky',
-                top: '0%',
-                left: '95.5%'
-              }}>
-              <Close />
-            </Fab>
-            {activeStep === 0 ? (
-              <Grid>
-                <Typography variant={'h3'} align={'center'} sx={{ mb: 1 }}>
-                  {'Kurse aus dem LMS'}
-                </Typography>
-                <TableCourse onCourseSelect={handleCourseSelection} />
-                <Grid container justifyContent="center" alignItems="center" sx={{ mt: 2 }}>
-                  <Button
-                    id="add-course-button"
-                    variant="contained"
-                    color="primary"
-                    onClick={() => setActiveStep(1)}>
-                    {'Select Course'}
-                  </Button>
-                </Grid>
-              </Grid>
-            ) : (
-              <Grid>
-                <Typography variant={'h3'} align={'center'} sx={{ mb: 1 }}>
-                  {'Hello'}
-                </Typography>
-                <TableCourseDetails course={selectedCourse}/>
-                <Grid container justifyContent="space-between" alignItems="center" sx={{ mt: 2 }}>
-                  <Button
-                    id="add-course-button"
-                    variant="contained"
-                    color="primary"
-                    onClick={() => setActiveStep(0)}>
-                    {'Back'}
-                  </Button>
-                  <Button
-                    id="add-course-button"
-                    variant="contained"
-                    color="primary"
-                    onClick={() => console.log('Selected Course:', selectedCourse)}>
-                    {'create Course'}
-                  </Button>
-                </Grid>
-              </Grid>
-            )
-            }
-          </Box>
-        </Grid>
-      </Modal>
-    )
+  const handleCourseSelection = (course: RemoteCourse) => {
+    setSelectedCourse(course)
   }
-)
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Box
+          sx={{
+            position: 'absolute',
+            left: '20%',
+            right: '20%',
+            top: '10%',
+            overflow: 'auto',
+            maxHeight: '83%',
+            bgcolor: 'background.paper',
+            border: (theme) => '2px solid' + theme.palette.secondary.dark,
+            boxShadow: 24,
+            p: 1
+          }}>
+          <Fab
+            color="primary"
+            data-testid={'QuestionnaireResultsCloseButton'}
+            onClick={handleClose}
+            sx={{
+              position: 'sticky',
+              top: '0%',
+              left: '95.5%'
+            }}>
+            <Close />
+          </Fab>
+          {activeStep === 0 ? (
+            <Grid>
+              <Typography variant={'h3'} align={'center'} sx={{ mb: 1 }}>
+                {'Kurse aus dem LMS'}
+              </Typography>
+              <TableCourse onCourseSelect={handleCourseSelection} />
+              <Grid container justifyContent="center" alignItems="center" sx={{ mt: 2 }}>
+                <Button id="add-course-button" variant="contained" color="primary" onClick={() => setActiveStep(1)}>
+                  {'Select Course'}
+                </Button>
+              </Grid>
+            </Grid>
+          ) : (
+            <Grid>
+              <Typography variant={'h3'} align={'center'} sx={{ mb: 1 }}>
+                {'Kursdetails'}
+              </Typography>
+              <TableCourseDetails course={selectedCourse} />
+              <Grid container justifyContent="space-between" alignItems="center" sx={{ mt: 2 }}>
+                <Button id="add-course-button" variant="contained" color="primary" onClick={() => setActiveStep(0)}>
+                  {'Back'}
+                </Button>
+                <Button
+                  id="add-course-button"
+                  variant="contained"
+                  color="primary"
+                  onClick={() => console.log('Selected Course:', selectedCourse)}>
+                  {'create Course'}
+                </Button>
+              </Grid>
+            </Grid>
+          )}
+        </Box>
+      </Grid>
+    </Modal>
+  )
+})
 // eslint-disable-next-line immutable/no-mutation
 CourseModal.displayName = 'CourseModal'
 export default CourseModal
--- src/components/Course/Table/TableCourseDetails.tsx
+++ src/components/Course/Table/TableCourseDetails.tsx
@@ -1,15 +1,85 @@
-import { RemoteCourse } from '@core'
-import { TextField, Grid, Typography, Select, MenuItem, InputLabel, FormControl } from '@common/components'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { useState } from 'react'
-import dayjs, { Dayjs } from 'dayjs'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import DefaultSave from '@mui/icons-material/Save'
+import DefaultSchool from '@mui/icons-material/School'
+import { FormControlLabel, SelectChangeEvent, Tooltip } from '@mui/material'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { SelectChangeEvent } from '@mui/material'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import dayjs, { Dayjs } from 'dayjs'
+import { useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  Divider,
+  Fab,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+  TextField,
+  Typography
+} from '@common/components'
+import { Close } from '@common/icons'
+import { RemoteCourse } from '@core'
+
+type optionsType = {
+  name: string
+  description: string
+  key: string
+}[]
 
 const TableCourseDetails = ({ course }: { course: RemoteCourse | undefined }) => {
   const [value, setValue] = useState<Dayjs | null>(dayjs('2022-04-17'))
-  const [algorithm, setAlgorithm] = useState('')
+  const [algorithm, setAlgorithm] = useState('10')
+
+  const [selected, setSelected] = useState(0)
+  const [teacherselection, setteacherselection] = useState(0)
+  const { t } = useTranslation()
+  const options = [
+    {
+      name: 'Fixed Order',
+      description: 'The learning elements are presented in a predetermined order.',
+      key: 'fixed'
+    },
+    {
+      name: 'Bayes',
+      description:
+        'Bayes is a probabilistic algorithm that calculates the learning path based on the probability for the best order for a learner.',
+      key: 'bayes'
+    },
+    {
+      name: 'Graf',
+      description:
+        'This algorithm is based on the learning adaptive mechanism by Graf et al. It calculates the learning path based on the learning style of the learner.',
+      key: 'graf'
+    },
+    {
+      name: 'ACO',
+      description:
+        'The Ant Colony Algorithm (ACO) is inspired by the behavior of ant workers. It calculates the learning path by simulating ants who leave behind pheromones to mark the best path.',
+      key: 'aco'
+    },
+    {
+      name: 'Genetic Algorithm',
+      description:
+        'The Genetic Algorithm is inspired by evolution. It approximates the best learning path by simulating the process of mutation and selection. It is often used for its speed.',
+      key: 'genetic'
+    },
+    {
+      name: 'Tyche',
+      description:
+        "Tyche is an algorithm based on the principle of luck. It is used to calculate the best learning path by taking into account the learners' luck factors.",
+      key: 'tyche'
+    }
+  ]
+  const handleSelect = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setSelected(parseInt(event.target.value))
+    },
+    [setSelected]
+  )
 
   const handleChange = (event: SelectChangeEvent) => {
     setAlgorithm(event.target.value as string)
@@ -17,24 +87,49 @@ const TableCourseDetails = ({ course }: { course: RemoteCourse | undefined }) =>
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Grid direction={'column'}>
-        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'} sx={{ mb: 3 }}>
-          <Typography>Kursname: </Typography>
-          <TextField defaultValue={course?.fullname} sx={{ minWidth: '30rem' }}></TextField>
+      <Grid container direction="column" spacing={3}>
+        <Grid item container direction="row" justifyContent="space-between" alignItems="center">
+          <TextField label="Kursname" defaultValue={course?.fullname} sx={{ width: '100%' }} />
         </Grid>
-        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'} sx={{ mb: 3 }}>
-          <Typography>Startdatum: </Typography>
-          <DatePicker label="Controlled picker" value={value} sx={{ minWidth: '30rem' }}></DatePicker>
+        <Grid item container direction="row" justifyContent="space-between" alignItems="center">
+          <DatePicker label="Startdatum" value={value} onChange={setValue} sx={{ width: '100%' }} />
         </Grid>
-        <Grid container direction={'row'} justifyContent={'space-between'} alignItems={'center'} sx={{ mb: 3 }}>
-          <Typography>Algorithmus: </Typography>
-          <FormControl>
-            <InputLabel>Standard-Algorithmus</InputLabel>
-            <Select value={algorithm} onChange={handleChange} sx={{ minWidth: '30rem' }} label={'StandardAlgorithmus'}>
-              <MenuItem value={10}>Standard-Algorithmus</MenuItem>
-              <MenuItem value={30}>Der Andere</MenuItem>
-            </Select>
-          </FormControl>
+        <Grid item container direction="row" alignItems="center" justifyContent="space-evenly">
+          <Grid item xs={3}>
+            <FormControl component="fieldset" sx={{ width: '100%', mt: 1 }}>
+              <InputLabel shrink sx={{ ml: '-3rem' }}>
+                Algorithmus
+              </InputLabel>
+              <RadioGroup onChange={handleSelect} value={selected} sx={{ mt: '1rem' }}>
+                {options.map((option, index) => (
+                  <Grid direction="row" key={option.key}>
+                    <FormControlLabel
+                      sx={{ width: { xl: '16rem' } }}
+                      value={index}
+                      control={<Radio role="radio-button" checked={index === selected} />}
+                      label={option.name}
+                    />
+                    {teacherselection === index && (
+                      <Tooltip
+                        arrow
+                        title={t('components.AlgorithmSettingsModal.teacherIconTip')}
+                        data-testid="teacher-icon">
+                        <DefaultSchool />
+                      </Tooltip>
+                    )}
+                  </Grid>
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+
+          <Divider orientation="vertical" flexItem sx={{ mt: '1.5rem' }} />
+
+          <Grid item xs={6}>
+            <Typography id="modal-description" variant="body1" component="p">
+              {options[selected].description}
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
     </LocalizationProvider>
