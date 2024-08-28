d31d672cd9832897280a8bdac56048a0b67a5940
DimitriB01 <d.bigler1996@gmx.de>
2024-08-28 10:07:40 +0200

    some translations and ids

--- src/components/CreateTopic/Table/TableTopics.tsx
+++ src/components/CreateTopic/Table/TableTopics.tsx
@@ -1,4 +1,5 @@
 import { memo } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Checkbox, FormControlLabel, FormGroup, Grid, Paper, Typography } from '@common/components'
 import { LearningPathTopic } from '@core'
 
@@ -7,12 +8,12 @@ type TableTopicsProps = {
 }
 
 const TableTopics = memo(({ topics }: TableTopicsProps) => {
-  console.log(topics)
+  const { t } = useTranslation()
   return (
     <Grid container direction="column" alignItems="center" spacing={3}>
       <Grid item>
         <Typography variant="h6" sx={{ mt: '1rem' }}>
-          Already created topics
+          {t('components.TableTopics')}
         </Typography>
       </Grid>
       <Grid item container>
@@ -23,6 +24,7 @@ const TableTopics = memo(({ topics }: TableTopicsProps) => {
                 control={<Checkbox disabled checked={true} />}
                 label={<Typography>{LmsTopic.name}</Typography>}
                 key={LmsTopic.name}
+                id={'topic-modal-already-created-topics-' + LmsTopic.name}
               />
             ))}
           </FormGroup>
