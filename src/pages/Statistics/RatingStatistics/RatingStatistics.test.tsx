b0ffaf91c70a553070901fe09b6c2d1692da4aa3
DimitriB01 <d.bigler1996@gmx.de>
2024-11-04 11:10:27 +0100

    fixing tests

--- src/pages/Statistics/RatingStatistics/RatingStatistics.test.tsx
+++ src/pages/Statistics/RatingStatistics/RatingStatistics.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { AuthContext } from '@services'
 import RatingStatistics from './RatingStatistics'
@@ -15,7 +15,7 @@ describe('RatingStatistics', () => {
     )
 
     const buttons = getAllByRole('button')
-    expect(buttons).toHaveLength(2)
+    expect(buttons).toHaveLength(3)
     expect(buttons[0]).toHaveAttribute('aria-pressed', 'true')
     expect(buttons[1]).toHaveAttribute('aria-pressed', 'false')
 
