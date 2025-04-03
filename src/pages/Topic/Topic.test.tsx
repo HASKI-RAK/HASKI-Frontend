cf80ea53d81570f0fc7da14c0ea03967dc5df9a7
DimitriB01 <d.bigler1996@gmx.de>
2025-04-03 19:55:37 +0200

    default learning path in topic, calculation fails in sending questionnaire

--- src/pages/Topic/Topic.test.tsx
+++ src/pages/Topic/Topic.test.tsx
@@ -1125,6 +1125,531 @@ describe('Topic Page', () => {
     })
   })
 
+  test('One le followed by a group of more than 3 le of the same classification, based on default learning Path', () => {
+    const { result } = renderHook(() => useTopic())
+    expect(result.current).toStrictEqual({
+      url: '',
+      title: '',
+      isOpen: false,
+      handleClose: expect.any(Function),
+      handleOpen: expect.any(Function),
+      mapNodes: expect.any(Function),
+      lmsId: -1
+    })
+
+    const mockLearningPath = {
+      id: 1,
+      course_id: 1,
+      based_on: 'default',
+      calculated_on: 'default',
+      path: [
+        {
+          id: 1,
+          learning_element_id: 1,
+          learning_path_id: 1,
+          recommended: true,
+          position: 1,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'EK',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 2,
+          learning_element_id: 1,
+          learning_path_id: 1,
+          recommended: true,
+          position: 2,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: '',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 3,
+          learning_element_id: 1,
+          learning_path_id: 1,
+          recommended: true,
+          position: 3,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 4,
+          learning_element_id: 2,
+          learning_path_id: 1,
+          recommended: true,
+          position: 4,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 5,
+          learning_element_id: 2,
+          learning_path_id: 1,
+          recommended: true,
+          position: 5,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 6,
+          learning_element_id: 2,
+          learning_path_id: 1,
+          recommended: true,
+          position: 6,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 7,
+          learning_element_id: 2,
+          learning_path_id: 1,
+          recommended: true,
+          position: 7,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        },
+        {
+          id: 8,
+          learning_element_id: 3,
+          learning_path_id: 1,
+          recommended: true,
+          position: 8,
+          learning_element: {
+            id: 1,
+            lms_id: 1,
+            activity_type: '',
+            classification: 'ÜB',
+            name: '',
+            university: '',
+            created_by: '',
+            created_at: '',
+            last_updated: '',
+            student_learning_element: {
+              id: 1,
+              student_id: 1,
+              learning_element_id: 1,
+              done: false,
+              done_at: ''
+            }
+          }
+        }
+      ]
+    }
+
+    const mockLearningElementStatus: LearningPathElementStatus[] = [
+      {
+        cmid: 1,
+        state: 0,
+        timecompleted: 1
+      },
+      {
+        cmid: 2,
+        state: 1,
+        timecompleted: 2
+      }
+    ]
+
+    const nodesAndEdges = result.current.mapNodes(
+      mockLearningPath,
+      mockLearningElementStatus,
+      mocklearningPathDisabledClassifications,
+      true
+    )
+    expect(nodesAndEdges).toStrictEqual({
+      nodes: [
+        {
+          data: {
+            activityType: '',
+            classification: 'EK',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            learningElementId: 1,
+            lmsId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: true,
+            handleSetLmsId: expect.any(Function)
+          },
+          id: '1',
+          position: {
+            x: -250,
+            y: 0
+          },
+          style: {
+            background: '#01579b',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'EK'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: '',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '2',
+          position: {
+            x: -250,
+            y: 250
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: ''
+        },
+        {
+          data: {
+            classification: 'ÜB',
+            label: 'components.NodeTypes.üb'
+          },
+          id: '3',
+          position: {
+            x: -1125,
+            y: 500
+          },
+          style: {
+            border: '2px solid #9e9e9e',
+            borderRadius: 8,
+            height: 325,
+            width: 2250
+          },
+          type: 'GROUP'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '3-1',
+          position: {
+            x: -1075,
+            y: 550
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '4-1',
+          position: {
+            x: -525,
+            y: 550
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '5-1',
+          position: {
+            x: 25,
+            y: 550
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '6-1',
+          position: {
+            x: 575,
+            y: 550
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: '',
+            isDone: false,
+            isDisabled: false
+          },
+          id: '7-1',
+          position: {
+            x: -1075,
+            y: 675
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        },
+        {
+          data: {
+            activityType: '',
+            classification: 'ÜB',
+            handleClose: expect.any(Function),
+            handleOpen: expect.any(Function),
+            handleSetLmsId: expect.any(Function),
+            handleSetTitle: expect.any(Function),
+            handleSetUrl: expect.any(Function),
+            isDone: false,
+            isDisabled: false,
+            isRecommended: true,
+            lmsId: 1,
+            learningElementId: 1,
+            name: ''
+          },
+          id: '8-1',
+          position: {
+            x: -525,
+            y: 675
+          },
+          style: {
+            background: '#1976d2',
+            border: '1px solid #9e9e9e',
+            borderRadius: 8,
+            cursor: 'pointer',
+            padding: 10,
+            width: 500
+          },
+          type: 'ÜB'
+        }
+      ],
+      edges: [
+        { id: 'Edge1', source: '1', target: '2' },
+        { id: 'Edge2', source: '2', target: '3' },
+        { id: 'Edge3', source: '3', target: undefined }
+      ]
+    })
+
+    act(() => {
+      result.current.handleOpen()
+      expect(result.current.isOpen).toBe(false)
+
+      result.current.handleClose()
+      expect(result.current.isOpen).toBe(false)
+    })
+  })
+
   test('General functionality of Topic hook with one le followed by a group of more than 3 le of the same classification', () => {
     const { result } = renderHook(() => useTopic())
     expect(result.current).toStrictEqual({
