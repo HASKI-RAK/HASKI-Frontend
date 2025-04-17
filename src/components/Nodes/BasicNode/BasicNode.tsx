da7125ffaf85d4b8aa03e1d0fe41edf199cc1b96
Gesine Wagner <80248709+Platura@users.noreply.github.com>
2025-04-17 14:59:53 +0200

    Update BasicNode

--- src/components/Nodes/BasicNode/BasicNode.tsx
+++ src/components/Nodes/BasicNode/BasicNode.tsx
@@ -16,6 +16,57 @@ type BasicNodeProps = NodeProps<LearningPathLearningElementNode> & {
 
 const BasicNode = ({ id, icon = <Feedback sx={{ fontSize: 50 }} />, ...props }: BasicNodeProps) => {
   const { t } = useTranslation()
+  const theme = useTheme()
+  const { addSnackbar } = useContext(SnackbarContext)
+  const { isCourseCreatorRole } = useContext(RoleContext)
+
+  const [deleteLearningElementModalOpen, setdeleteLearningElementModalOpen] = useState(false)
+  const [learningElementName, setLearningElementName] = useState<string>('')
+  const [learningElementId, setLearningElementId] = useState<number>(0)
+  const [lmsLearningElementId, setLmsLearningElementId] = useState<number>(0)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const clearLearningPathElement = useStore((state) => state.clearLearningPathElementCache)
+  const clearLearningPathElementStatusCache = usePersistedStore((state) => state.clearLearningPathElementStatusCache)
+
+  const onMouseEnter = () => {
+    setIsHovered(true)
+  }
+  const onMouseLeave = () => {
+    setIsHovered(false)
+  }
+
+  // Handle node click but ignore clicks that originated from the delete icon.
+  const handleNodeClick = (event: MouseEvent) => {
+    if ((event.target as HTMLElement).closest('.learning-element-delete-icon')) {
+      return // Skip the iframe action if it came from the delete button.
+    }
+    props.data.handleOpen()
+    props.data.handleSetUrl(getConfig().MOODLE + `/mod/${props.data.activityType}/view.php?id=${props.data.lmsId}`)
+    props.data.handleSetLmsId(props.data.lmsId)
+  }
+
+  const handleOpenDeleteLearningElementModal = () => {
+    setdeleteLearningElementModalOpen(true)
+    setLearningElementName(props.data.name)
+    setLearningElementId(props.data.learningElementId)
+    setLmsLearningElementId(props.data.lmsId)
+    setIsHovered(false)
+  }
+
+  const handleAcceptDeleteLearningElementModal = (learningElementId: number, lmsLearningElementId: number) => {
+    deleteLearningElement(learningElementId, lmsLearningElementId).then(() => {
+      addSnackbar({
+        message: t('components.BasicNode.deleteLearningElementSuccessful'),
+        severity: 'success',
+        autoHideDuration: 5000
+      })
+      setdeleteLearningElementModalOpen(false)
+    })
+    clearLearningPathElement()
+    clearLearningPathElementStatusCache()
+  }
+
   return (
     <NodeWrapper
       id={`${id}-${props.data.lmsId}`}
