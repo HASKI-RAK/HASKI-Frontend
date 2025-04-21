30c6ae0cfa786e084b1a1bbbb77aff7743b8e8b6
Julian Manz <106922840+juli-txt@users.noreply.github.com>
2025-04-21 21:36:20 +0200

    linted

--- src/core/Rating/LearningElementRating.ts
+++ src/core/Rating/LearningElementRating.ts
@@ -1,17 +1,17 @@
 type LearningElementRating = {
-    learning_element_id: number
-    topic_id: number
-    rating_value: number
-    rating_deviation: number
-    timestamp: Date
-  }
-  
-  type LearningElementRatingResponse = LearningElementRating[]
-  
-  type LearningElementRatingReturn = (
-    learningElementId?: number,
-    topicId?: number
-  ) => Promise<LearningElementRatingResponse>
-  
-  export default LearningElementRating
-  export type { LearningElementRatingReturn, LearningElementRatingResponse }
+  learning_element_id: number
+  topic_id: number
+  rating_value: number
+  rating_deviation: number
+  timestamp: Date
+}
+
+type LearningElementRatingResponse = LearningElementRating[]
+
+type LearningElementRatingReturn = (
+  learningElementId?: number,
+  topicId?: number
+) => Promise<LearningElementRatingResponse>
+
+export default LearningElementRating
+export type { LearningElementRatingReturn, LearningElementRatingResponse }
