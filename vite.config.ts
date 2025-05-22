5dd0fea9554ab1e77e5705c7739e045558b2b3b0
DimitriB01 <d.bigler1996@gmx.de>
2025-05-22 12:07:57 +0200

    upgrading vite and webpack

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true
   },
+  server: {
+    allowedHosts: ['fakedomain.com']
+  },
   publicDir: '../public',
   plugins: [tsconfigPaths(), react()]
 })
