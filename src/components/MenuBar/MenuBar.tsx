8537a782065acfcd2f016c233056a58977a44659
DimitriB01 <d.bigler1996@gmx.de>
2025-10-17 11:45:58 +0200

    new and final tutorial document for HASKI

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -237,7 +237,7 @@ const MenuBar = () => {
               <IconButton
                 id="manual-icon-button"
                 onClick={() => {
-                  window.open('/files/Tutorial_zur_Bedienung_von_HASKI_Okt24.pdf', '_blank')
+                  window.open('/files/Bedienung_von_HASKI.pdf', '_blank')
                 }}>
                 <Help data-testid="HelpIcon" />
               </IconButton>
