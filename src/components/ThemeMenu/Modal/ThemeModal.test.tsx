c9cfebeb5e78ee9805e2b74d2b97dab1b898740d
DimitriB01 <d.bigler1996@gmx.de>
2025-12-11 13:49:15 +0100

    Revert "merged requirements"
    This reverts commit 24d52e8aae7173d34eeb0509803f7064e3209bf3.

--- src/components/ThemeMenu/Modal/ThemeModal.test.tsx
+++ src/components/ThemeMenu/Modal/ThemeModal.test.tsx
@@ -12,7 +12,7 @@ jest.mock('react-i18next', () => ({
   })
 }))
 
-describe('[HASKI-REQ-0070] ThemeModal tests', () => {
+describe('[HASKI-REQ-0073] ThemeModal tests', () => {
   it('should render ThemeModal component correctly in closed state', () => {
     const themeModal = render(
       <ThemeProvider>
